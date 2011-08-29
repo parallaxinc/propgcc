ae3856827858ddfa875ac8f56d57aed70de3107a
Eric Smith <ersmith@totalspectrum.ca>
2011-08-29 18:45:14 -0300

    started configuring libgcc

--- gcc/gcc/config/propeller/propeller.h
+++ gcc/gcc/config/propeller/propeller.h
@@ -585,12 +585,4 @@ extern const char *propeller_bss_asm_op;
 
 #define CONSTANT_POOL_BEFORE_FUNCTION (0)
 
-/* some variables controlling output of constants and functions */
-extern bool propeller_need_mulsi;
-extern bool propeller_need_udivsi;
-extern bool propeller_need_divsi;
-extern bool propeller_need_clzsi;
-extern bool propeller_need_mask0000ffff;
-extern bool propeller_need_maskffffffff;
-
 #endif /* GCC_PROPELLER_H */
