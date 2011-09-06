16398657a0148526bb33ce24ee3f9f7d04269bdb
Eric Smith <ersmith@totalspectrum.ca>
2011-09-05 21:42:14 -0300

    added malloc definition

---
+++ binutils/bfd/elf32-propeller.c
@@ -0,0 +1,460 @@
+/* propeller-specific support for 32-bit ELF.
+   Copyright 2011 Parallax Inc.
+
+   Copied from elf32-moxie.c which is..
+   Copyright 2009, 2010 Free Software Foundation, Inc.
+
+   This file is part of BFD, the Binary File Descriptor library.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston,
+   MA 02110-1301, USA.  */
+
+#include "sysdep.h"
+#include "bfd.h"
+#include "libbfd.h"
+#include "elf-bfd.h"
+#include "elf/propeller.h"
+
+/* Forward declarations.  */
+
+static reloc_howto_type propeller_elf_howto_table[] = {
+  /* This reloc does nothing. */
+  HOWTO (R_PROPELLER_NONE,	/* type */
+	 0,			/* rightshift */
+	 2,			/* size (0 = byte, 1 = short, 2 = long) */
+	 32,			/* bitsize */
+	 FALSE,			/* pc_relative */
+	 0,			/* bitpos */
+	 complain_overflow_dont,	/* complain_on_overflow */
+	 bfd_elf_generic_reloc,	/* special_function */
+	 "R_PROPELLER_NONE",	/* name */
+	 FALSE,			/* partial_inplace */
+	 0,			/* src_mask */
+	 0,			/* dst_mask */
+	 FALSE),		/* pcrel_offset */
+
+  /* A 32 bit absolute relocation. */
+  HOWTO (R_PROPELLER_32,	/* type */
+	 0,			/* rightshift */
+	 2,			/* size (0 = byte, 1 = short, 2 = long) */
+	 32,			/* bitsize */
+	 FALSE,			/* pc_relative */
+	 0,			/* bitpos */
+	 complain_overflow_dont,	/* complain_on_overflow */
+	 bfd_elf_generic_reloc,	/* special_function */
+	 "R_PROPELLER_32",	/* name */
+	 FALSE,			/* partial_inplace */
+	 0x00000000,		/* src_mask */
+	 0xffffffff,		/* dst_mask */
+	 FALSE),		/* pcrel_offset */
+
+  /* A 16 bit absolute relocation. */
+  HOWTO (R_PROPELLER_16,	/* type */
+	 0,			/* rightshift */
+	 1,			/* size (0 = byte, 1 = short, 2 = long) */
+	 16,			/* bitsize */
+	 FALSE,			/* pc_relative */
+	 0,			/* bitpos */
+	 complain_overflow_bitfield,	/* complain_on_overflow */
+	 bfd_elf_generic_reloc,	/* special_function */
+	 "R_PROPELLER_16",	/* name */
+	 FALSE,			/* partial_inplace */
+	 0x00000000,		/* src_mask */
+	 0x0000ffff,		/* dst_mask */
+	 FALSE),		/* pcrel_offset */
+
+  /* An 8 bit absolute relocation. */
+  HOWTO (R_PROPELLER_8,	        /* type */
+	 0,			/* rightshift */
+	 0,			/* size (0 = byte, 1 = short, 2 = long) */
+	 8,			/* bitsize */
+	 FALSE,			/* pc_relative */
+	 0,			/* bitpos */
+	 complain_overflow_bitfield,	/* complain_on_overflow */
+	 bfd_elf_generic_reloc,	/* special_function */
+	 "R_PROPELLER_8",	/* name */
+	 FALSE,			/* partial_inplace */
+	 0x00000000,		/* src_mask */
+	 0x000000ff,		/* dst_mask */
+	 FALSE),		/* pcrel_offset */
+
+  /* A 9 bit relocation of the SRC field of an instruction */
+  /* this one is an immediate constant rather than an address,
+     so do not right shift it by 2
+  */
+  HOWTO (R_PROPELLER_SRC_IMM,	/* type */
+	 0,			/* rightshift */
+	 2,			/* size (0 = byte, 1 = short, 2 = long) */
+	 9,			/* bitsize */
+	 FALSE,			/* pc_relative */
+	 0,			/* bitpos */
+	 complain_overflow_bitfield,	/* complain_on_overflow */
+	 bfd_elf_generic_reloc,	/* special_function */
+	 "R_PROPELLER_SRC_IMM",	/* name */
+	 FALSE,			/* partial_inplace */
+	 0x00000000,		/* src_mask */
+	 0x000001FF,		/* dst_mask */
+	 FALSE),		/* pcrel_offset */
+
+  /* A 9 bit relocation of the SRC field of an instruction */
+  HOWTO (R_PROPELLER_SRC,	/* type */
+	 2,			/* rightshift */
+	 2,			/* size (0 = byte, 1 = short, 2 = long) */
+	 9,			/* bitsize */
+	 FALSE,			/* pc_relative */
+	 0,			/* bitpos */
+	 complain_overflow_bitfield,	/* complain_on_overflow */
+	 bfd_elf_generic_reloc,	/* special_function */
+	 "R_PROPELLER_SRC",	/* name */
+	 FALSE,			/* partial_inplace */
+	 0x00000000,		/* src_mask */
+	 0x000001FF,		/* dst_mask */
+	 FALSE),		/* pcrel_offset */
+
+  /* A 9 bit relocation of the DST field of an instruction */
+  HOWTO (R_PROPELLER_DST,	/* type */
+	 2,			/* rightshift */
+	 2,			/* size (0 = byte, 1 = short, 2 = long) */
+	 9,			/* bitsize */
+	 FALSE,			/* pc_relative */
+	 9,			/* bitpos */
+	 complain_overflow_bitfield,	/* complain_on_overflow */
+	 bfd_elf_generic_reloc,	/* special_function */
+	 "R_PROPELLER_DST",	/* name */
+	 FALSE,			/* partial_inplace */
+	 0x00000000,		/* src_mask */
+	 0x0003FE00,		/* dst_mask */
+	 FALSE),		/* pcrel_offset */
+
+};
+
+/* Map BFD reloc types to Propeller ELF reloc types. */
+
+struct propeller_reloc_map
+{
+  bfd_reloc_code_real_type bfd_reloc_val;
+  unsigned int propeller_reloc_val;
+};
+
+static const struct propeller_reloc_map propeller_reloc_map[] = {
+  {BFD_RELOC_NONE, R_PROPELLER_NONE},
+  {BFD_RELOC_32, R_PROPELLER_32},
+  {BFD_RELOC_16, R_PROPELLER_16},
+  {BFD_RELOC_8, R_PROPELLER_8},
+  {BFD_RELOC_PROPELLER_SRC, R_PROPELLER_SRC},
+  {BFD_RELOC_PROPELLER_SRC_IMM, R_PROPELLER_SRC_IMM},
+  {BFD_RELOC_PROPELLER_DST, R_PROPELLER_DST},
+};
+
+static reloc_howto_type *
+propeller_reloc_type_lookup (bfd * abfd ATTRIBUTE_UNUSED,
+			     bfd_reloc_code_real_type code)
+{
+  unsigned int i;
+
+  for (i = sizeof (propeller_reloc_map) / sizeof (propeller_reloc_map[0]);
+       --i;)
+    if (propeller_reloc_map[i].bfd_reloc_val == code)
+      return &propeller_elf_howto_table[propeller_reloc_map[i].
+					propeller_reloc_val];
+
+  return NULL;
+}
+
+static reloc_howto_type *
+propeller_reloc_name_lookup (bfd * abfd ATTRIBUTE_UNUSED, const char *r_name)
+{
+  unsigned int i;
+
+  for (i = 0;
+       i <
+       sizeof (propeller_elf_howto_table) /
+       sizeof (propeller_elf_howto_table[0]); i++)
+    if (propeller_elf_howto_table[i].name != NULL
+	&& strcasecmp (propeller_elf_howto_table[i].name, r_name) == 0)
+      return &propeller_elf_howto_table[i];
+
+  return NULL;
+}
+
+/* Set the howto pointer for an PROPELLER ELF reloc.  */
+
+static void
+propeller_info_to_howto_rela (bfd * abfd ATTRIBUTE_UNUSED,
+			      arelent * cache_ptr, Elf_Internal_Rela * dst)
+{
+  unsigned int r_type;
+
+  r_type = ELF32_R_TYPE (dst->r_info);
+  BFD_ASSERT (r_type < (unsigned int) R_PROPELLER_max);
+  cache_ptr->howto = &propeller_elf_howto_table[r_type];
+}
+
+/* Perform a single relocation.  By default we use the standard BFD
+   routines, but a few relocs, we have to do them ourselves.  */
+
+static bfd_reloc_status_type
+propeller_final_link_relocate (reloc_howto_type * howto,
+			       bfd * input_bfd,
+			       asection * input_section,
+			       bfd_byte * contents,
+			       Elf_Internal_Rela * rel, bfd_vma relocation)
+{
+  bfd_reloc_status_type r = bfd_reloc_ok;
+
+  switch (howto->type)
+    {
+    case R_PROPELLER_SRC_IMM:
+    case R_PROPELLER_SRC:
+    case R_PROPELLER_DST:
+    default:
+      r = _bfd_final_link_relocate (howto, input_bfd, input_section,
+				    contents, rel->r_offset,
+				    relocation, rel->r_addend);
+    }
+
+  return r;
+}
+
+/* Relocate an PROPELLER ELF section.
+
+   The RELOCATE_SECTION function is called by the new ELF backend linker
+   to handle the relocations for a section.
+
+   The relocs are always passed as Rela structures; if the section
+   actually uses Rel structures, the r_addend field will always be
+   zero.
+
+   This function is responsible for adjusting the section contents as
+   necessary, and (if using Rela relocs and generating a relocatable
+   output file) adjusting the reloc addend as necessary.
+
+   This function does not have to worry about setting the reloc
+   address or the reloc symbol index.
+
+   LOCAL_SYMS is a pointer to the swapped in local symbols.
+
+   LOCAL_SECTIONS is an array giving the section in the input file
+   corresponding to the st_shndx field of each local symbol.
+
+   The global hash table entry for the global symbols can be found
+   via elf_sym_hashes (input_bfd).
+
+   When generating relocatable output, this function must handle
+   STB_LOCAL/STT_SECTION symbols specially.  The output symbol is
+   going to be the section symbol corresponding to the output
+   section, which means that the addend must be adjusted
+   accordingly.  */
+
+static bfd_boolean
+propeller_elf_relocate_section (bfd * output_bfd,
+				struct bfd_link_info *info,
+				bfd * input_bfd,
+				asection * input_section,
+				bfd_byte * contents,
+				Elf_Internal_Rela * relocs,
+				Elf_Internal_Sym * local_syms,
+				asection ** local_sections)
+{
+  Elf_Internal_Shdr *symtab_hdr;
+  struct elf_link_hash_entry **sym_hashes;
+  Elf_Internal_Rela *rel;
+  Elf_Internal_Rela *relend;
+
+  symtab_hdr = &elf_tdata (input_bfd)->symtab_hdr;
+  sym_hashes = elf_sym_hashes (input_bfd);
+  relend = relocs + input_section->reloc_count;
+
+  for (rel = relocs; rel < relend; rel++)
+    {
+      reloc_howto_type *howto;
+      unsigned long r_symndx;
+      Elf_Internal_Sym *sym;
+      asection *sec;
+      struct elf_link_hash_entry *h;
+      bfd_vma relocation;
+      bfd_reloc_status_type r;
+      const char *name;
+      int r_type;
+
+      r_type = ELF32_R_TYPE (rel->r_info);
+      r_symndx = ELF32_R_SYM (rel->r_info);
+      howto = propeller_elf_howto_table + r_type;
+      h = NULL;
+      sym = NULL;
+      sec = NULL;
+
+      if (r_symndx < symtab_hdr->sh_info)
+	{
+	  sym = local_syms + r_symndx;
+	  sec = local_sections[r_symndx];
+	  if( (r_type == R_PROPELLER_SRC_IMM) && 
+	      0 != (sym->st_other & PROPELLER_OTHER_COG_RAM) )
+	    {
+	      Elf_Internal_Sym s = *sym;
+	      s.st_value /= 4;
+	      relocation = _bfd_elf_rela_local_sym (output_bfd, &s, &sec, rel);
+	    }
+	  else
+	    {
+	      relocation = _bfd_elf_rela_local_sym (output_bfd, sym, &sec, rel);
+	    }
+
+	  name = bfd_elf_string_from_elf_section
+	    (input_bfd, symtab_hdr->sh_link, sym->st_name);
+	  name = (name == NULL) ? bfd_section_name (input_bfd, sec) : name;
+	}
+      else
+	{
+	  bfd_boolean unresolved_reloc, warned;
+
+	  RELOC_FOR_GLOBAL_SYMBOL (info, input_bfd, input_section, rel,
+				   r_symndx, symtab_hdr, sym_hashes,
+				   h, sec, relocation,
+				   unresolved_reloc, warned);
+
+	  name = h->root.root.string;
+	}
+
+      if (sec != NULL && elf_discarded_section (sec))
+	RELOC_AGAINST_DISCARDED_SECTION (info, input_bfd, input_section,
+					 rel, relend, howto, contents);
+
+      if (info->relocatable)
+	continue;
+
+      r = propeller_final_link_relocate (howto, input_bfd, input_section,
+					 contents, rel, relocation);
+
+      if (r != bfd_reloc_ok)
+	{
+	  const char *msg = NULL;
+
+	  switch (r)
+	    {
+	    case bfd_reloc_overflow:
+	      r = info->callbacks->reloc_overflow
+		(info, (h ? &h->root : NULL), name, howto->name,
+		 (bfd_vma) 0, input_bfd, input_section, rel->r_offset);
+	      break;
+
+	    case bfd_reloc_undefined:
+	      r = info->callbacks->undefined_symbol
+		(info, name, input_bfd, input_section, rel->r_offset, TRUE);
+	      break;
+
+	    case bfd_reloc_outofrange:
+	      msg = _("internal error: out of range error");
+	      break;
+
+	    case bfd_reloc_notsupported:
+	      msg = _("internal error: unsupported relocation error");
+	      break;
+
+	    case bfd_reloc_dangerous:
+	      msg = _("internal error: dangerous relocation");
+	      break;
+
+	    default:
+	      msg = _("internal error: unknown error");
+	      break;
+	    }
+
+	  if (msg)
+	    r = info->callbacks->warning
+	      (info, msg, name, input_bfd, input_section, rel->r_offset);
+
+	  if (!r)
+	    return FALSE;
+	}
+    }
+
+  return TRUE;
+}
+
+/* Return the section that should be marked against GC for a given
+   relocation.  */
+
+static asection *
+propeller_elf_gc_mark_hook (asection * sec,
+			    struct bfd_link_info *info,
+			    Elf_Internal_Rela * rel,
+			    struct elf_link_hash_entry *h,
+			    Elf_Internal_Sym * sym)
+{
+  return _bfd_elf_gc_mark_hook (sec, info, rel, h, sym);
+}
+
+/* Look through the relocs for a section during the first phase.
+   Since we don't do .gots or .plts, we just need to consider the
+   virtual table relocs for gc.  */
+
+static bfd_boolean
+propeller_elf_check_relocs (bfd * abfd,
+			    struct bfd_link_info *info,
+			    asection * sec, const Elf_Internal_Rela * relocs)
+{
+  Elf_Internal_Shdr *symtab_hdr;
+  struct elf_link_hash_entry **sym_hashes;
+  const Elf_Internal_Rela *rel;
+  const Elf_Internal_Rela *rel_end;
+
+  if (info->relocatable)
+    return TRUE;
+
+  symtab_hdr = &elf_tdata (abfd)->symtab_hdr;
+  sym_hashes = elf_sym_hashes (abfd);
+
+  rel_end = relocs + sec->reloc_count;
+  for (rel = relocs; rel < rel_end; rel++)
+    {
+      struct elf_link_hash_entry *h;
+      unsigned long r_symndx;
+
+      r_symndx = ELF32_R_SYM (rel->r_info);
+      if (r_symndx < symtab_hdr->sh_info)
+	h = NULL;
+      else
+	{
+	  h = sym_hashes[r_symndx - symtab_hdr->sh_info];
+	  while (h->root.type == bfd_link_hash_indirect
+		 || h->root.type == bfd_link_hash_warning)
+	    h = (struct elf_link_hash_entry *) h->root.u.i.link;
+	}
+    }
+
+  return TRUE;
+}
+
+#define ELF_ARCH		bfd_arch_propeller
+#define ELF_MACHINE_CODE	EM_PROPELLER
+#define ELF_MAXPAGESIZE		0x1
+
+#define TARGET_LITTLE_SYM          bfd_elf32_propeller_vec
+#define TARGET_LITTLE_NAME		"elf32-propeller"
+
+#define elf_info_to_howto_rel			NULL
+#define elf_info_to_howto			propeller_info_to_howto_rela
+#define elf_backend_relocate_section		propeller_elf_relocate_section
+#define elf_backend_gc_mark_hook		propeller_elf_gc_mark_hook
+#define elf_backend_check_relocs                propeller_elf_check_relocs
+
+#define elf_backend_can_gc_sections		1
+#define elf_backend_rela_normal			1
+
+#define bfd_elf32_bfd_reloc_type_lookup		propeller_reloc_type_lookup
+#define bfd_elf32_bfd_reloc_name_lookup		propeller_reloc_name_lookup
+
+#include "elf32-target.h"
---
+++ loader/spin/flash_loader.spin
@@ -0,0 +1,164 @@
+CON
+
+  _clkmode = XTAL1 + PLL16X
+  _clkfreq = 80000000
+
+  hub_memory_size = 32 * 1024
+  cache_mbox_size = cacheint#_MBOX_SIZE * 4
+  
+  FLASH_BASE        = $30000000
+  
+  HDR_ENTRY         = 0
+  HDR_BSS_START     = 4
+  HDR_BSS_END       = 8
+  HDR_DATA_IMAGE    = 12
+  HDR_DATA_START    = 16
+  HDR_DATA_END      = 20
+
+OBJ
+
+  cacheint : "cache_interface"
+
+PUB start | cache
+
+  ' initialize the cache driver
+  cache := hub_memory_size - p_cache_size
+  cache_mbox := cache - cache_mbox_size
+  cache_line_mask := cacheint.start(@cache_code, cache_mbox, cache, p_cache_param1, p_cache_param2)
+
+   ' start the xmm kernel boot code
+  coginit(cogid, @boot, @vm_code)
+
+DAT
+
+                    org     0
+
+' parameters filled in before downloading flash_loader.binary
+boot                jmp     #boot_next
+p_cache_size        long    0
+p_cache_param1      long    0
+p_cache_param2      long    0
+p_vm_code_off       long    @vm_code - @boot
+p_cache_code_off    long    @cache_code - @boot
+
+cache_mbox          long    0
+cache_mbox_dat      long    0
+cache_line_mask     long    0
+
+src                 long    0
+dst                 long    0
+count               long    0
+coginit_dest        long    0
+temp                long    0
+
+entry               long     FLASH_BASE + HDR_ENTRY
+bss_start           long     FLASH_BASE + HDR_BSS_START
+bss_end             long     FLASH_BASE + HDR_BSS_END
+data_start          long     FLASH_BASE + HDR_DATA_START
+data_end            long     FLASH_BASE + HDR_DATA_END
+
+'Copy PAR block into COG
+boot_next
+                    'clear .bss
+                    mov     t1, bss_start
+                    call    #read_long
+                    mov     dst, t1
+                    mov     t1, bss_end
+                    call    #read_long
+                    mov     count, t1
+                    sub     count, dst
+                    shr     count, #2 wz
+        if_z        jmp     #:done_bss
+                    mov     t2, #0
+:loop_bss           wrlong  t2, dst
+                    add     dst, #4
+                    djnz    count, #:loop_bss
+:done_bss
+
+                    'initialize .data
+                    mov     t1, data_start
+                    call    #read_long
+                    mov     dst, t1
+                    mov     t1, data_end
+                    call    #read_long
+                    mov     count, t1
+                    sub     count, dst
+                    shr     count, #2 wz
+        if_z        jmp     #:done_data
+:loop_data          mov     t1, src
+                    call    #read_long
+                    wrlong  t1, dst
+                    add     src, #4
+                    add     dst, #4
+                    djnz    count, #:loop_data
+:done_data
+
+                    mov     dst, cache_mbox
+                    mov     t1, entry
+                    call    #read_long
+                    sub     dst, #4
+                    wrlong  t1, dst
+                    sub     dst, #4
+                    wrlong  cache_line_mask, dst
+                    sub     dst, #4
+                    wrlong  cache_mbox, dst
+
+'Launch the xmm kernel in this cog.
+launch              cogid   coginit_dest            'Get id of this cog for restarting
+                    mov     temp, PAR               'Set pasm code address for coginit
+                    shl     temp, #2                'Shift into bits 17:4 (only high 14 bits required)
+                    or      coginit_dest, temp      'Place in dest for coginit
+                    mov     temp, dst               'Set PAR to sp for coginit
+                    shl     temp, #16               'Move to bits 31:18 (only high 14 bits required)
+                    or      coginit_dest, temp      'Combine PASM addr and PAR addr
+                    coginit coginit_dest            'Start the XMM kernel
+
+read_long           call    #cache_read
+                    rdlong  t1, memp
+read_long_ret       ret
+
+write_long          call    #cache_write
+                    wrlong  t2, memp
+write_long_ret      ret
+
+cache_write         mov     memp, t1                    'save address for index
+                    andn    t1, #cacheint#CMD_MASK      'ensure a write is not a read
+                    or      t1, #cacheint#WRITE_CMD
+                    jmp     #cache_access
+
+cache_read          mov     temp, t1                    'ptr + cache_mboxdat = hub address of byte to load
+                    andn    temp, cache_linemask
+                    cmp     cacheaddr, temp wz          'if cacheaddr == addr, just pull form cache
+        if_ne       jmp     #:next                      'memp gets overwriteen on a miss
+                    mov     memp, t1                    'ptr + cache_mboxdat = hub address of byte to load
+                    and     memp, cache_linemask
+                    add     memp, cacheptr              'add ptr to memp to get data address
+                    jmp     #cache_read_ret
+:next               mov     memp, t1                    'save address for index
+                    or      t1, #cacheint#READ_CMD      'read must be 3 to avoid needing andn addr,#cache#CMD_MASK
+
+cache_access        wrlong  t1, cache_mbox
+                    mov     cacheaddr,t1                'Save new cache address. it's free time here
+                    andn    cacheaddr,cache_linemask    'Kill command bits in free time
+:waitres            rdlong  temp, cache_mbox wz
+        if_nz       jmp     #:waitres
+                    and     memp, cache_linemask        'memp is index into buffer
+                    rdlong  cacheptr, cache_mbox_dat    'Get new buffer
+                    add     memp, cacheptr              'memp is now HUB buf address of data to read
+cache_read_ret
+cache_write_ret     ret
+
+t1                  long    0
+t2                  long    0
+cache_linemask      long    0
+cache_mboxcmd       long    0
+cache_mboxdat       long    0
+memp                long    0
+cacheaddr           long    0
+cacheptr            long    0
+
+                    fit     496
+
+vm_code             long    0[496]
+cache_code          long    0[496]
+
