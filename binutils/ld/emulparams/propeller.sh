c49bda64ea8eb99e99d1e4578b3d3d23da0a3b73
Ken Rose <rose@acm.org>
2011-09-10 19:15:43 -0700

    Merge

--- binutils/ld/emulparams/propeller.sh
+++ binutils/ld/emulparams/propeller.sh
@@ -1,9 +1,19 @@
-SCRIPT_NAME=elf
-OUTPUT_FORMAT="elf32-propeller"
-TEXT_START_ADDR=0
-TARGET_PAGE_SIZE=4
 ARCH=propeller
-TEMPLATE_NAME=elf32
-GENERATE_SHLIB_SCRIPT=yes
-GENERATE_PIE_SCRIPT=yes
+SCRIPT_NAME=propeller
+OUTPUT_FORMAT="elf32-propeller"
 EMBEDDED=yes
+TEMPLATE_NAME=elf32
+EXTRA_EM_FILE=propeller
+
+KERNEL="
+  /* the LMM kernel that is loaded into the cog */
+  .lmmkernel ${RELOCATING-0} :
+  {
+    *(.lmmkernel)
+    *(.kernel)
+  } >cog AT>hub
+"
+KERNEL_NAME=.lmmkernel
+TEXT_MEMORY=">hub AT>hub"
+HUBTEXT_MEMORY=">hub AT>hub"
+DATA_MEMORY=">hub"
