54d11b1f585683d1f704cd858429faa685b38735
Eric Smith <ersmith@totalspectrum.ca>
2011-09-18 08:30:30 -0300

    fixed a bunch of issues with propeller stubs; still not working yet

--- newlib/src/newlib/libc/sys/propeller/exec.c
+++ newlib/src/newlib/libc/sys/propeller/exec.c
@@ -2,7 +2,7 @@
 #include <errno.h>
 
 int
-_execve_r(struct _reent *reent, char *name, char **argv, char **env)
+_execve_r(struct _reent *reent, const char *name, char * const *argv, char * const *env)
 {
     reent->_errno = ENOSYS;
     return -1;
