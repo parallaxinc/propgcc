93867620c63622b2ef55696c6072eb843fe7b3c1
David Betz <dbetz@xlisper.com>
2011-09-20 23:52:56 -0400

    Add filesystem code to open and close.

--- newlib/src/newlib/libc/sys/propeller/close.c
+++ newlib/src/newlib/libc/sys/propeller/close.c
@@ -1,9 +1,24 @@
 #include <errno.h>
 #include <reent.h>
+#include "propdev.h"
+
+extern _fs_state_t _fs_state;
 
 int _close_r(struct _reent *reent, int fd)
 {
-  reent->_errno = EIO;
-  return -1;
+    int i = fd - _FS_BASE_FD;
+    if (!_fs_state.initialized) {
+        reent->_errno = EIO;
+        return -1;
+    }
+    if (i < 0 || i >= _FS_MAX_FILES) {
+        reent->_errno = EBADF;
+        return -1;
+    }
+    if (!_fs_state.files[i].inUse) {
+        reent->_errno = EINVAL;
+        return -1;
+    }
+    _fs_state.files[i].inUse = FALSE;
+    return 0;
 }
-
