c5e32422d22f13c920219927738dd2992656b98f
Eric Smith <ersmith@totalspectrum.ca>
2011-09-24 20:21:01 -0300

    more multilib build work

--- gcc/gcc/config/propeller/propeller.h
+++ gcc/gcc/config/propeller/propeller.h
@@ -26,7 +26,7 @@
 /* Config for gas and binutils   */
 /*-------------------------------*/
 #undef  STARTFILE_SPEC
-#define STARTFILE_SPEC "%{mxmm*:hubstart_xmm.o%s; :spinboot.o%s} %{_crt0.o%s _crtbegin.o%s}"
+#define STARTFILE_SPEC "%{mxmm*:hubstart_xmm.o%s; :spinboot.o%s} _crt0.o%s _crtbegin.o%s"
 
 #undef  ENDFILE_SPEC
 #define ENDFILE_SPEC "_crtend.o%s"
