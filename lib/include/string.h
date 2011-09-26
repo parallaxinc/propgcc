99118347d93175a309f1ec2432936ccd55d42b07
Eric Smith <ersmith@totalspectrum.ca>
2011-09-26 12:05:05 -0300

    added some more string functions

--- lib/include/string.h
+++ lib/include/string.h
@@ -1,8 +1,7 @@
 #ifndef _STRING_H
 #define _STRING_H
 
-#include <sys/size_t.h>
-#include <sys/null.h>
+#include <stddef.h>
 
 size_t strlen(const char *s);
 char * strcat(char *dest, const char *src);
@@ -13,7 +12,12 @@ char * strncpy(char *dest, const char *src, size_t n);
 int    strcmp(const char *s1, const char *s2);
 int    strncmp(const char *s1, const char *s2, size_t n);
 
+char *strchr(const char *, int);
+char *strrchr(const char *, int);
+
 void *memcpy(void *dest, const void *src, size_t n);
 void *memset(void *dest, int c, size_t n);
+void *memchr(const void *s, int c, size_t n);
+int   memcmp(const void *s1, const void *s2, size_t n);
 
 #endif
