0b365ecf2625c8f6adc987bc4386d575cc2dce51
David Betz <dbetz@xlisper.com>
2011-10-03 17:48:17 -0400

    Added macros to load COG drivers from external memory.

---
+++ demos/toggle/cog_c_toggle/toggle_cog.c
@@ -0,0 +1,58 @@
+/*
+ * code to toggle pins
+ * this is the cog C version of the code; it runs in a separate
+ * cog from the LMM kernel
+ * Copyright (c) 2011 Parallax, Inc.
+ * MIT Licensed (see at end of file for exact terms)
+ */
+
+#include "cog.h"
+#include "toggle.h"
+
+/*
+ * our local variables (placed in cog memory) for speed
+ */
+static _COGMEM unsigned int waitdelay;
+static _COGMEM unsigned int pins = 0x3fffffff;  /* all pins */
+static _COGMEM unsigned int nextcnt;
+
+_NATIVE
+void main (volatile struct toggle_mailbox *m)
+{
+  /* get a half second delay from parameters */
+  _DIRA = pins;
+
+  /* figure out how long to wait the first time */
+  nextcnt = _CNT + m->wait_time;
+
+  /* loop forever, updating the wait time from the mailbox */
+  for(;;) {
+    waitdelay = m->wait_time;
+    _OUTA ^= pins;
+    nextcnt = __builtin_propeller_waitcnt(nextcnt, waitdelay);
+  }
+}
+
+/* +--------------------------------------------------------------------
+ * ¦  TERMS OF USE: MIT License
+ * +--------------------------------------------------------------------
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files
+ * (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * +--------------------------------------------------------------------
+ */
