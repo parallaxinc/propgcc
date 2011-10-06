384586fbadc49c5be8e46171136734ec5d6aedac
Eric Smith <ersmith@totalspectrum.ca>
2011-10-06 10:23:18 -0300

    update libpropeller to handle new crt0

---
+++ demos/common/demo.mk
@@ -0,0 +1,90 @@
+# #########################################################
+# This makefile fragment builds LMM/XMM/XMMC demo programs
+#   
+# To use it, define:
+#  PROPLIB to be the path to this directory
+#  NAME to be the name of project
+#       - this is used to create the final program $(NAME).elf
+#  OBJS to be the object files needed for the project
+#  MODEL to lmm, xmm, or xmmc
+#  CFLAGS to be desired CFLAGS
+#
+#  Then set up a default "all" target (normally this will be
+#    all: $(NAME).elf
+#  and finally
+#    include $(PROPLIB)/demo.mk
+#
+# Copyright (c) 2011 Parallax Inc.
+# All rights MIT licensed
+# #########################################################
+
+# where we installed the propeller binaries and libraries
+PREFIX = /usr/local/propeller
+
+# libgcc directory
+LIBGCC = $(PREFIX)/lib/gcc/propeller-elf/4.6.1
+
+ifndef MODEL
+MODEL=lmm
+endif
+
+ifndef PORT
+PORT=1
+endif
+
+ifndef BOARD
+BOARD=hub
+endif
+
+
+CFLAGS += -m$(MODEL)
+LDFLAGS = -m$(MODEL)
+
+# basic gnu tools
+CC = propeller-elf-gcc
+CXX = propeller-elf-g++
+LD = propeller-elf-ld
+AS = propeller-elf-as
+OBJCOPY = propeller-elf-objcopy
+CHKSUM = propeller-checksum
+LOADER = propeller-load
+
+CXXFLAGS += $(CFLAGS)
+
+# BSTC program
+BSTC=bstc -ls
+SPINDIR=.
+ECHO=echo
+
+
+$(NAME).elf: $(OBJS)
+	$(CC) $(LDFLAGS) -o $@ $(OBJS)
+
+%.o: %.c
+	$(CC) $(CFLAGS) -o $@ -c $<
+
+%.o: %.s
+	$(CC) -o $@ -c $<
+
+%.binary: %.elf
+	$(LOADER) -s $<
+
+%.dat: $(SPINDIR)/%.spin
+	$(BSTC) -Ox -c -o $(basename $@) $<
+	$(ECHO) $@
+
+%_firmware.o: %.dat
+	$(OBJCOPY) -I binary -B propeller -O $(CC) $< $@
+	rm -rf $<
+	$(ECHO) $@
+
+clean:
+	rm -f *.o *.elf *.cog *.binary
+
+#
+# how to run
+#
+run: $(NAME).elf
+	$(LOADER) -p$(PORT) -b$(BOARD) $(NAME).elf -r -t
+
+.FORCE:
