1f5b85b452c3f604f4cc2c5e2114b6ccc481b277
Eric Smith <ersmith@totalspectrum.ca>
2011-10-06 21:18:52 -0300

    added time_t definition

--- lib/include/time.h
+++ lib/include/time.h
@@ -2,10 +2,13 @@
 #define _TIME_H
 
 typedef unsigned int clock_t;
+typedef unsigned int time_t;
+
 extern clock_t _clkfreq;
 
 #define CLOCKS_PER_SEC _clkfreq
 
 clock_t clock(void);
+time_t  time(time_t *);
 
 #endif
