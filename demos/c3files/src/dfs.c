90b7092f1bb4d03ed88ceb80553ca09bba769cd5
Dave Hein <davehein3@gmail.com>
2011-11-01 15:59:52 -0500

    Adding c3files to demos

---
+++ demos/c3files/src/dfs.c
@@ -0,0 +1,439 @@
+/*
+# #########################################################
+# This file contains the interface code for the DOSFS FAT
+# file sytem driver.  The code is separated into three main
+# sections, which consist of the standard file I/O driver,
+# the basic file routines that call the DOSFS fucntions,
+# and the low level sector read and write routines that are
+# called by DOSFS.
+#   
+# Written by Dave Hein with contributions from other GCC
+# development team members.
+# Copyright (c) 2011 Parallax, Inc.
+# MIT Licensed
+# #########################################################
+*/
+
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/driver.h>
+#include <compiler.h>
+#include <errno.h>
+#include <propeller.h>
+#include "dosfs.h"
+#include "dfs.h"
+
+#if defined(__PROPELLER_XMMC__) || defined(__PROPELLER_XMM__)
+#define CACHE_SIZE 512
+#else
+#define CACHE_SIZE 32
+#endif
+
+static VOLINFO volinfo;
+static int mountflag = 0;
+static __attribute__((section(".hub"))) uint8_t scratch[512];
+
+//#####################################
+// Device driver
+//#####################################
+extern _Driver _SimpleSerialDriver;
+
+const char FilePrefix[] = "";
+
+_Driver FileDriver =
+  {
+    FilePrefix,
+    File_fopen,
+    File_fclose,
+    File_read,
+    File_write,
+    NULL,  /* seek; not applicable */
+    NULL,  /* remove; not applicable */
+  };
+
+/* This is a list of all drivers we can use in the
+ * program. The default _InitIO function opens stdin,
+ * stdout, and stderr based on the first driver in
+ * the list (the serial driver, for us)
+ */
+_Driver *_driverlist[] = {
+  &_SimpleSerialDriver,
+  &FileDriver,
+  NULL
+};
+
+/* function called by fopen */
+int File_fopen(FILE *fp, const char *str, const char *mode)
+{
+    PFILEINFO dfs_fp = (PFILEINFO)dfs_open(str, mode);
+    fp->drvarg[0] = (int)dfs_fp;
+#ifdef CACHE_SIZE
+    if (dfs_fp)
+    {
+        fp->_ptr = fp->_base = ((void *)dfs_fp) + sizeof(FILEINFO);
+        fp->_bsiz = CACHE_SIZE;
+        fp->_flag |= _IOFREEBUF;
+    }
+#endif
+    return (int)dfs_fp;
+}
+
+/* function called by fclose */
+int File_fclose(FILE *fp)
+{
+    PFILEINFO dfs_fp = (PFILEINFO)fp->drvarg[0];
+    return dfs_close(dfs_fp);
+}
+
+/* function called by fwrite */
+int File_write(FILE *fp, unsigned char *buf, int count)
+{
+    PFILEINFO dfs_fp = (PFILEINFO)fp->drvarg[0];
+    return dfs_write(dfs_fp, buf, count);
+}
+
+/* function called by fread */
+int File_read(FILE *fp, unsigned char *buf, int count)
+{
+    PFILEINFO dfs_fp = (PFILEINFO)fp->drvarg[0];
+    return dfs_read(dfs_fp, buf, count);
+}
+
+//#####################################
+// DOSFS interface routines
+//#####################################
+static void error(char *str)
+{
+    fprintf(stderr, "%s\n", str);
+    exit(1);
+}
+
+static void warning(char *str)
+{
+    fprintf(stderr, "%s\n", str);
+}
+
+uint32_t dfs_read(PFILEINFO fileinfo, uint8_t *buffer, uint32_t num)
+{
+    int retval;
+    uint32_t successcount;
+
+    retval = DFS_ReadFile(fileinfo, scratch, buffer, &successcount, num);
+    if (retval != DFS_OK) successcount = 0;
+    return successcount;
+}
+
+uint32_t dfs_write(PFILEINFO fileinfo, uint8_t *buffer, uint32_t num)
+{
+    int retval;
+    uint32_t successcount;
+
+    retval = DFS_WriteFile(fileinfo, scratch, buffer, &successcount, num);
+    if (retval != DFS_OK) successcount = 0;
+    return successcount;
+}
+
+uint32_t dfs_mount()
+{
+    int retval;
+
+    if (mountflag) return 0;
+
+    InitFileIO();
+    MountFS();
+    retval = DFS_OK;
+    mountflag = (retval == DFS_OK); 
+    return retval;
+}
+
+PFILEINFO dfs_open(const char *fname, const char *mode)
+{
+    int retval;
+    int modeflag;
+    PFILEINFO fileinfo;
+
+    if (!mountflag)
+        return 0;
+
+#ifdef CACHE_SIZE
+    fileinfo = malloc(sizeof(FILEINFO) + CACHE_SIZE);
+#else
+    fileinfo = malloc(sizeof(FILEINFO));
+#endif
+
+    if (!fileinfo)
+        return 0;
+
+    if (mode[0] == 'w' && mode[1] != 'a')
+    {
+        retval = DFS_OpenFile(&volinfo, (char *)fname, DFS_READ, scratch, fileinfo);
+        if (retval == DFS_OK)
+            DFS_UnlinkFile(&volinfo, (char *)fname, scratch);
+        retval = DFS_OpenFile(&volinfo, (char *)fname, DFS_WRITE | DFS_READ, scratch, fileinfo);
+    }
+    else if (mode[0] == 'w' && mode[1] == 'a')
+    {
+        retval = DFS_OpenFile(&volinfo, (char *)fname, DFS_WRITE | DFS_READ, scratch, fileinfo);
+        if (retval == DFS_OK)
+            DFS_Seek(fileinfo, 0xffffffff, scratch);
+    }
+    else
+    {
+        retval = DFS_OpenFile(&volinfo, (char *)fname, DFS_READ, scratch, fileinfo);
+    }
+
+    if (retval != DFS_OK)
+    {
+        free(fileinfo);
+        fileinfo = 0;
+    }
+
+    return fileinfo;
+}
+
+int dfs_close(PFILEINFO fileinfo)
+{
+    free(fileinfo);
+    return 0;
+}
+
+PDIRINFO dfs_opendir(char *dirname)
+{
+    int i;
+    int retval;
+    PDIRINFO dirinfo = malloc(sizeof(DIRINFO) + sizeof(DIRENT) + SECTOR_SIZE);
+
+    if (!dirinfo)
+        return 0;
+
+    dirinfo->scratch = ((void *)dirinfo) + sizeof(DIRINFO) + sizeof(DIRENT);
+    retval = DFS_OpenDir(&volinfo, dirname, dirinfo);
+
+    if (retval == DFS_OK)
+        return dirinfo;
+
+    free(dirinfo);
+
+    return 0;
+}
+
+void dfs_closedir(PDIRINFO dirinfo)
+{
+    if (dirinfo)
+        free(dirinfo);
+}
+
+PDIRENT dfs_readdir(PDIRINFO dirinfo)
+{
+    int i;
+    int retval;
+    PDIRENT dirent = ((void *)dirinfo) + sizeof(DIRINFO);
+
+    while (1)
+    {
+        retval = DFS_GetNext(&volinfo, dirinfo, dirent);
+        if (retval != DFS_OK) return 0;
+        if (dirent->name[0]) break;
+    }
+
+    return dirent;
+}
+
+int dfs_remove(char *fname)
+{
+    return DFS_UnlinkFile(&volinfo, fname, scratch);
+}
+
+//#####################################
+// Low level routines for initialization
+// and reading and writing sectors.
+//#####################################
+#undef  FS_DEBUG
+
+#ifndef TRUE
+#define TRUE    1
+#define FALSE   0
+#endif
+
+volatile uint32_t *xmm_mbox;
+
+#if !defined(__PROPELLER_XMMC__) && !defined(__PROPELLER_XMM__)
+#define CACHE_ADDR  (32*1024 - 1*1024)
+#define XMM_MBOX    (CACHE_ADDR - 2*sizeof(uint32_t))
+#endif
+
+static void LoadCacheDriver(void)
+{
+#if defined(__PROPELLER_XMMC__) || defined(__PROPELLER_XMM__)
+    extern uint16_t _xmm_mbox_p;
+    xmm_mbox = (uint32_t *)(uint32_t)_xmm_mbox_p;
+#else
+    extern uint8_t c3_cache_array[];
+    uint32_t params[4];
+    params[0] = XMM_MBOX;
+    params[1] = CACHE_ADDR;
+    params[2] = 0;
+    params[3] = 0;
+    cognew(c3_cache_array, params);
+    xmm_mbox = (uint32_t *)XMM_MBOX;
+    while (xmm_mbox[0])
+        ;
+#endif
+}
+
+int MountFS(void)
+{
+    int i;
+    uint32_t start, err;
+
+    // Find the first sector of the volume
+    DFS_ReadSector(0, scratch, 0, 1);
+    if (!strncmp(scratch+0x36, "FAT16", 5) ||
+        !strncmp(scratch+0x52, "FAT32", 5))
+        start = 0;
+    else
+        start = scratch[0x1c6] | (scratch[0x1c7] << 8) |
+                (scratch[0x1c8] << 16) | (scratch[0x1c9] << 24);
+
+#ifdef FS_DEBUG
+    printf("GetVolInfo:\r\n");
+#endif
+    err = DFS_GetVolInfo(0, scratch, start, &volinfo);
+#ifdef FS_DEBUG
+    printf(" -->%ld\r\n", err);
+#endif
+    if (err != DFS_OK)
+        return FALSE;
+
+#ifdef FS_DEBUG
+    printf("Volume label '%-11.11s'\r\n", volinfo.label);
+    printf("%d sector/s per cluster, %d reserved sector/s, volume total %ld sectors.\r\n",
+                volinfo.secperclus,
+                volinfo.reservedsecs,
+                volinfo.numsecs);
+    printf("%ld sectors per FAT, first FAT at sector #%ld, root dir at #%ld.\r\n",
+                volinfo.secperfat,
+                volinfo.fat1,
+                volinfo.rootdir);
+    printf("(For FAT32, the root dir is a CLUSTER number, FAT12/16 it is a SECTOR number)\r\n");
+    printf("%d root dir entries, data area commences at sector #%ld.\r\n",
+                volinfo.rootentries,
+                volinfo.dataarea);
+    printf("%ld clusters (%ld bytes) in data area, filesystem IDd as ",
+                volinfo.numclusters,
+                volinfo.numclusters * volinfo.secperclus * SECTOR_SIZE);
+    if (volinfo.filesystem == FAT12)
+        printf("FAT12.\r\n");
+    else if (volinfo.filesystem == FAT16)
+        printf("FAT16.\r\n");
+    else if (volinfo.filesystem == FAT32)
+        printf("FAT32.\r\n");
+    else {
+        printf("[unknown]\r\n");
+        return FALSE;
+    }
+#endif
+
+    return TRUE;
+}
+
+#define SD_INIT_CMD             0x0d
+#define SD_READ_CMD             0x11
+#define SD_WRITE_CMD            0x15
+
+static uint32_t __attribute__((section(".hubtext"))) do_cmd(uint32_t cmd)
+{
+    xmm_mbox[0] = cmd;
+    while (xmm_mbox[0]);
+    return xmm_mbox[1];
+}
+
+int InitFileIO(int retries)
+{
+    uint32_t result;
+    LoadCacheDriver();
+    while (retries == 0 || --retries >= 0) {
+        result = do_cmd(SD_INIT_CMD);
+        if (result == 0)
+            return 0;
+        printf("Retrying SD init: %d\n", result);
+    }
+    return -1;
+}
+
+uint32_t DFS_ReadSector(uint8_t unit, uint8_t *buffer, uint32_t sector, uint32_t count)
+{
+    uint32_t params[3], result;
+    while (count > 0) {
+        if (((uint32_t)buffer) & 0xffff0000)
+            params[0] = (uint32_t)scratch;
+        else
+            params[0] = (uint32_t)buffer;
+        params[1] = SECTOR_SIZE;
+        params[2] = sector;
+        result = do_cmd(SD_READ_CMD | ((uint32_t)params << 8));
+        if (result != 0) {
+            printf("SD_READ_CMD failed: %d\n", result);
+            return -1;
+        }
+        if (((uint32_t)buffer) & 0xffff0000)
+            memcpy(buffer, scratch, SECTOR_SIZE);
+        buffer += SECTOR_SIZE;
+        ++sector;
+        --count;
+    }
+
+    return 0;
+}
+
+uint32_t DFS_WriteSector(uint8_t unit, uint8_t *buffer, uint32_t sector, uint32_t count)
+{
+    uint32_t params[3], result;
+    while (count > 0) {
+        if (((uint32_t)buffer) & 0xffff0000)
+        {
+            memcpy(scratch, buffer, SECTOR_SIZE);
+            params[0] = (uint32_t)scratch;
+        }
+        else
+            params[0] = (uint32_t)buffer;
+        params[1] = SECTOR_SIZE;
+        params[2] = sector;
+        result = do_cmd(SD_WRITE_CMD | ((uint32_t)params << 8));
+        if (result != 0) {
+            printf("SD_WRITE_CMD failed: %d\n", result);
+            return -1;
+        }
+        buffer += SECTOR_SIZE;
+        ++sector;
+        --count;
+    }
+    return 0;
+}
+
+/*
++--------------------------------------------------------------------
+¦  TERMS OF USE: MIT License
++--------------------------------------------------------------------
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files
+(the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge,
+publish, distribute, sublicense, and/or sell copies of the Software,
+and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
++------------------------------------------------------------------
+*/
---
+++ demos/c3files/src/filetest.c
@@ -0,0 +1,400 @@
+/*
+############################################################################
+# This program is used to test the basic functions of the SD file system.
+# It implements simple versions of the cat, rm, ls and echo commands plus
+# the <, > and >> file redirection operators.
+# The program starts up the file driver and then prompts for a command.
+#
+# Written by Dave Hein
+# Copyright (c) 2011 Parallax, Inc.
+# MIT Licensed
+############################################################################
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <cog.h>
+#include <propeller.h>
+#include "dfs.h"
+
+FILE *stdinfile;
+FILE *stdoutfile;
+
+/* Print help information */
+void Help()
+{
+    printf("Available commands are help, cat, rm, ls and echo\n");
+}
+
+/* This routine implements the file cat function */
+void Cat(int argc, char **argv)
+{
+    int i;
+    int num;
+    void *infile;
+    uint8_t buffer[40];
+
+    for (i = 0; i < argc; i++)
+    {
+        if (i == 0)
+        {
+            if (argc == 1 || stdinfile != stdin)
+                infile = stdinfile;
+            else
+                continue;
+        }
+        else
+        {
+            infile = fopen(argv[i], "r");
+            if (infile == 0)
+            {
+                printf("Could not open file %s\n", argv[i]);
+                continue;
+            }
+        }
+        if (infile == stdin)
+        {
+            while (gets(buffer))
+            {
+                if (buffer[0] == 4) break;
+                fprintf(stdoutfile, "%s\n", buffer);
+            }
+        }
+        else
+        {
+            while ((num = fread(buffer, 1, 40, infile)))
+                fwrite(buffer, 1, num, stdoutfile);
+        }
+        if (i)
+            fclose(infile);
+    }
+    fflush(stdout);
+}
+
+/* This routine deletes the files specified by the command line arguments */
+void Remove(int argc, char **argv)
+{
+    int i;
+    for (i = 1; i < argc; i++)
+    {
+        if (dfs_remove(argv[i]))
+            printf("Cannot delete %s\n", argv[i]);
+    }
+}
+
+/* This routine echos the command line arguments */
+void Echo(int argc, char **argv)
+{
+    int i;
+    for (i = 1; i < argc; i++)
+    {
+        if (i != argc - 1)
+            fprintf(stdoutfile, "%s ", argv[i]);
+        else
+            fprintf(stdoutfile, "%s\n", argv[i]);
+    }
+}
+
+/* This routine lists the root directory or any subdirectories specified
+   in the command line arguments.  If the "-l" option is specified, it
+   will print the file attributes and size.  Otherwise, it will just
+   print the file names.  */
+void List(int argc, char **argv)
+{
+    int i, j;
+    char *ptr;
+    char fname[13];
+    PDIRENT dirent;
+    int32_t count = 0;
+    uint32_t filesize;
+    uint32_t longflag = 0;
+    char *path = argv[1];
+    PDIRINFO dirinfo;
+    char drwx[5];
+    int column;
+    int prevlen;
+
+    // Check flags
+    for (j = 1; j < argc; j++)
+    {
+        if (argv[j][0] == '-')
+        {
+            if (!strcmp(argv[j], "-l"))
+                longflag = 1;
+            else
+                fprintf(stdoutfile, "Unknown option \"%s\"\n", argv[j]);
+        }
+        else
+            count++;
+    }
+
+    // List directories
+    for (j = 1; j < argc || count == 0; j++)
+    {
+        if (count == 0)
+        {
+            count--;
+            path = "";
+        }
+        else if (argv[j][0] == '-')
+            continue;
+        else
+            path = argv[j];
+
+        if (count >= 2)
+            fprintf(stdoutfile, "\n%s:\n", path);
+
+        dirinfo = dfs_opendir(path);
+
+        if (!dirinfo)
+        {
+            fprintf(stdoutfile, "Could not open %s\n", path);
+            continue;
+        }
+
+        column = 0;
+        prevlen = 14;
+        while (dirent = dfs_readdir(dirinfo))
+        {
+            if (dirent->name[0] == '.') continue;
+            ptr = fname;
+            for (i = 0; i < 8; i++)
+            {
+                if (dirent->name[i] == ' ') break;
+                *ptr++ = dirent->name[i];
+            }
+            if (dirent->name[8] != ' ')
+            {
+                *ptr++ = '.';
+                for (i = 8; i < 11; i++)
+                {
+                    if (dirent->name[i] == ' ') break;
+                    *ptr++ = dirent->name[i];
+                }
+            }
+            *ptr = 0;
+            filesize = dirent->filesize_3;
+            filesize = (filesize << 8) | dirent->filesize_2;
+            filesize = (filesize << 8) | dirent->filesize_1;
+            filesize = (filesize << 8) | dirent->filesize_0;
+            strcpy(drwx, "-rw-");
+            if (dirent->attr & 0x10)
+            {
+                drwx[0] = 'd';
+                drwx[3] = 'x';
+            }
+            if (longflag)
+                fprintf(stdoutfile, "%s %8d %s\n", drwx, filesize, fname);
+            else if (++column == 5)
+            {
+                for (i = prevlen; i < 14; i++) fprintf(stdoutfile, " ");
+                fprintf(stdoutfile, "%s\n", fname);
+                column = 0;
+                prevlen = 14;
+            }
+            else
+            {
+                for (i = prevlen; i < 14; i++) fprintf(stdoutfile, " ");
+                prevlen = strlen(fname);
+                fprintf(stdoutfile, "%s", fname);
+            }
+        }
+        dfs_closedir(dirinfo);
+        if (!longflag && column)
+            fprintf(stdoutfile, "\n");
+    }
+}
+
+/* This routine returns a pointer to the first character that doesn't
+   match val. */
+char *SkipChar(char *ptr, int val)
+{
+    while (*ptr)
+    {
+        if (*ptr != val) break;
+        ptr++;
+    }
+    return ptr;
+}
+
+/* This routine returns a pointer to the first character that matches val. */
+char *FindChar(char *ptr, int val)
+{
+    while (*ptr)
+    {
+        if (*ptr == val) break;
+        ptr++;
+    }
+    return ptr;
+}
+
+/* This routine extracts tokens from a string that are separated by one or
+   more spaces.  It returns the number of tokens found. */
+int tokenize(char *ptr, char *tokens[])
+{
+    int num = 0;
+
+    while (*ptr)
+    {
+        ptr = SkipChar(ptr, ' ');
+        if (*ptr == 0) break;
+        if (ptr[0] == '>')
+        {
+            ptr++;
+            if (ptr[0] == '>')
+            {
+                tokens[num++] = ">>";
+                ptr++;
+            }
+            else
+                tokens[num++] = ">";
+            continue;
+        }
+        if (ptr[0] == '<')
+        {
+            ptr++;
+            tokens[num++] = "<";
+            continue;
+        }
+        tokens[num++] = ptr;
+        ptr = FindChar(ptr, ' ');
+        if (*ptr) *ptr++ = 0;
+    }
+    return num;
+}
+
+/* This routine searches the list of tokens for the redirection operators
+   and opens the files for input, output or append depending on the 
+   operator. */
+int CheckRedirection(char **tokens, int num)
+{
+    int i, j;
+
+    for (i = 0; i < num-1; i++)
+    {
+        if (!strcmp(tokens[i], ">"))
+        {
+            stdoutfile = fopen(tokens[i+1], "w");
+            if (!stdoutfile)
+            {
+                printf("Could not open %s\n", tokens[i+1]);
+                stdoutfile = stdout;
+                return 0;
+            }
+        }
+        else if (!strcmp(tokens[i], ">>"))
+        {
+            // Open the file with "wa" instead of "a" to work around a
+            // problem with the "a" mode.
+            stdoutfile = fopen(tokens[i+1], "wa");
+            if (!stdoutfile)
+            {
+                printf("Could not open %s\n", tokens[i+1]);
+                stdoutfile = stdout;
+                return 0;
+            }
+        }
+        else if (!strcmp(tokens[i], "<"))
+        {
+            stdinfile = fopen(tokens[i+1], "r");
+            if (!stdinfile)
+            {
+                printf("Could not open %s\n", tokens[i+1]);
+                stdinfile = stdin;
+                return 0;
+            }
+        }
+        else
+            continue;
+        for (j = i + 2; j < num; j++) tokens[j-2] = tokens[j];
+        i--;
+        num -= 2;
+    }
+    return num;
+}
+
+/* This routine closes files that were open for redirection */
+void CloseRedirection()
+{
+    if (stdinfile != stdin)
+    {
+        fclose(stdinfile);
+        stdinfile = stdin;
+    }
+    if (stdoutfile != stdout)
+    {
+        fclose(stdoutfile);
+        stdoutfile = stdout;
+    }
+}
+
+/* The program starts the file system.  It then loops reading commands
+   and calling the appropriate routine to process it. */
+int main()
+{
+    int num;
+    char *tokens[20];
+    uint8_t buffer[80];
+
+    stdinfile = stdin;
+    stdoutfile = stdout;
+
+    dfs_mount();
+
+    printf("\n");
+    Help();
+
+    while (1)
+    {
+        printf("\n> ");
+        fflush(stdout);
+        gets(buffer);
+        num = tokenize(buffer, tokens);
+        num = CheckRedirection(tokens, num);
+        if (num == 0) continue;
+        if (!strcmp(tokens[0], "help"))
+            Help();
+        else if (!strcmp(tokens[0], "cat"))
+            Cat(num, tokens);
+        else if (!strcmp(tokens[0], "ls"))
+            List(num, tokens);
+        else if (!strcmp(tokens[0], "rm"))
+            Remove(num, tokens);
+        else if (!strcmp(tokens[0], "echo"))
+            Echo(num, tokens);
+        else
+        {
+            printf("Invalid command\n");
+            Help();
+        }
+        CloseRedirection();
+    }
+
+    return 0;
+}
+/*
++--------------------------------------------------------------------
+|  TERMS OF USE: MIT License
++--------------------------------------------------------------------
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files
+(the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge,
+publish, distribute, sublicense, and/or sell copies of the Software,
+and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
++------------------------------------------------------------------
+*/
+
