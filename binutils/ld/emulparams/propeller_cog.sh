38f0c72ef9b9a40330da3ef21a67473b655274f8
Eric Smith <ersmith@totalspectrum.ca>
2011-11-18 22:54:34 -0400

    fix -mcog building of simple programs

--- binutils/ld/emulparams/propeller_cog.sh
+++ binutils/ld/emulparams/propeller_cog.sh
@@ -5,7 +5,7 @@ EMBEDDED=yes
 TEMPLATE_NAME=elf32
 EXTRA_EM_FILE=propeller
 
-TEXT_MEMORY=">cog"
+TEXT_MEMORY=">cog AT>hub"
 DATA_MEMORY=">hub AT>hub"
 DATA_BSS_MEMORY=">hub AT>hub"
 HUBTEXT_MEMORY=">hub AT>hub"
