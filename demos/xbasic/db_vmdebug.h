40e9067a6f1f19f36aaf3236c705cfb975d32014
David Betz <dbetz@xlisper.com>
2011-11-22 17:33:21 -0500

    Remove the xbasic demo. It only uses 16 bit integers and doesn't make sense on the Propeller.

--- demos/xbasic/db_vmdebug.h
+++
@@ -1,9 +0,0 @@
-#ifndef __DB_VMDEBUG_H__
-#define __DB_VMDEBUG_H__
-
-#include "db_types.h"
-
-void DecodeFunction(uint16_t offset, const uint8_t *code, int len);
-int DecodeInstruction(uint16_t base, const uint8_t *code, const uint8_t *lc);
-
-#endif
