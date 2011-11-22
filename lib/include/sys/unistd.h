f9374e535df0ce6d695b700f0e378194cec88ae9
Steve Denson <jsdenson@gmail.com>
2011-11-22 10:28:25 -0800

    Added fflush(stdout) for netbeans to work. added MIT license.

---
+++ lib/include/sys/unistd.h
@@ -0,0 +1,15 @@
+#ifndef _SYS_UNISTD_H
+#define _SYS_UNISTD_H
+
+typedef long off_t;
+
+int write(int fd, const void *buf, int count);
+int read(int fd, const void *buf, int count);
+off_t lseek(int fd, off_t offset, int whence);
+unsigned int sleep(unsigned int seconds);
+
+char *getcwd(char *buf, int size);
+int chdir(const char *path);
+int rmdir(const char *path);
+
+#endif
---
+++ lib/stdlib/llabs.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+
+long long
+llabs(long long n)
+{
+  return (n < 0) ? -n : n;
+}
