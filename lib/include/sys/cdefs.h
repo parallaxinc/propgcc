c1ec3e548e032d2d3f97dd56b217e5bcedd04521
Eric Smith <ersmith@totalspectrum.ca>
2011-11-24 22:01:35 -0400

    fix definition of __weak_alias to include the underscore we need

--- lib/include/sys/cdefs.h
+++ lib/include/sys/cdefs.h
@@ -1,17 +1,6 @@
 #ifndef _SYS_CDEFS_H
 #define _SYS_CDEFS_H
 
-#if defined(__GNUC__)
-#ifndef __weak_alias
-#define __weak_alias(sym, oldfunc) \
-  __asm__( " .weak " #sym "\n  .equ " #sym "," #oldfunc "\n" )
-#endif
-#ifndef __strong_alias
-#define __strong_alias(sym, oldfunc) \
-  __asm__( " .global " #sym "\n  .equ " #sym "," #oldfunc "\n" )
-#endif
-#else
-#error "must define __weak_alias and __strong_alias"
-#endif
+#include <compiler.h>
 
 #endif
