6f428fe94bf8ebaebb71a29676439305df05338e
David Betz <dbetz@xlisper.com>
2011-11-27 23:19:21 -0500

    Attempt at fixing the VGA colors in demos/Term.

--- demos/Term/lib/launch.c
+++ demos/Term/lib/launch.c
@@ -3,12 +3,6 @@
 
 #define EXTERNAL_START  0x20000000
 
-static void longcopy(uint32_t *dst, uint32_t *src, int len)
-{
-    while (--len >= 0)
-        *dst++ = *src++;
-}
-
 int launchcog(uint32_t *code, uint32_t *end, void *param)
 {
     int id;
@@ -20,7 +14,7 @@ int launchcog(uint32_t *code, uint32_t *end, void *param)
     // handle code in external memory
     else {
         uint32_t buffer[496]; // this assumes the stack is always in hub memory!!
-        longcopy(buffer, code, end - code);
+        copy_from_xmm(buffer, code, end - code);
         id = cognew((uint32_t)buffer, (uint32_t)param);
     }
     
