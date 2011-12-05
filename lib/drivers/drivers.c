2c453eba55fe9d1b861e3060061035af67ff6e08
Eric Smith <ersmith@totalspectrum.ca>
2011-12-05 13:18:53 -0400

    use memcpy to copy from XMM memory to hub

--- lib/drivers/drivers.c
+++ lib/drivers/drivers.c
@@ -1,15 +1,16 @@
 #if defined(__PROPELLER_XMM__) || defined(__PROPELLER_XMMC__)
 
 #include <stdint.h>
+#include <string.h>
 #include "propdev.h"
 
-uint32_t __attribute__ ((section(".hub"))) hub_buffer[496];
+uint32_t __attribute__ ((section(".hub"))) _hub_buffer[496];
 
 int __attribute__((section(".hubtext"))) load_cog_driver_xmm(uint32_t *code, uint32_t codelen, uint32_t *param)
 {
     if (codelen > 496) return -1;
-    copy_from_xmm(hub_buffer, code, codelen);
-    return cognew(hub_buffer, param);
+    memcpy(_hub_buffer, code, codelen*4);
+    return cognew(_hub_buffer, param);
 }
 
 #endif
