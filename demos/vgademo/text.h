0a3f20e9cc34ef7e189668ff781a7ae0a1de7397
Dave Hein <davehein3@gmail.com>
2012-01-30 20:00:05 -0600

    Adding vgademo

---
+++ demos/vgademo/text.h
@@ -0,0 +1,5 @@
+#define CHAR_HEIGHT 11
+#define CHAR_WIDTH  8
+
+void PutCharImage(int col, int row, int value, int scale, int vert, int color);
+void PutStringImage(int col, int row, char *str, int scale, int vert, int color);
---
+++ demos/vgademo/text2.h
@@ -0,0 +1,7 @@
+#define CHAR_HEIGHT2 32
+#define CHAR_WIDTH2  16
+
+#define uint unsigned int
+
+void PutCharImage2(int col, int row, int value, int scale, int vert, int color);
+void PutStringImage2(int col, int row, char *str, int scale, int vert, int color);
