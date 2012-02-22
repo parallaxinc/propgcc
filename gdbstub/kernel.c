f410ec3538e81f7fe3350fc7b0687d5c7049ec1e
David Betz <dbetz@xlisper.com>
2012-02-21 19:05:54 -0500

    Fix up the code formatting a bit.

--- gdbstub/kernel.c
+++ gdbstub/kernel.c
@@ -1,3 +1,24 @@
+/* kernel.c - a dummy debug kernel for the gdb stub used for initial downloads
+
+Copyright (c) 2012 David Michael Betz
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
 #include <stdint.h>
 #include "propeller.h"
 
@@ -36,7 +57,7 @@ _NAKED int main(void)
     _DIRA &= ~rxmask;
     
     for (;;) {
-
+        
         txbyte(HALT);
         
         while (rxbyte() != ATTN)
@@ -44,19 +65,19 @@ _NAKED int main(void)
         txbyte(ACK);
         
         switch (rxbyte()) {
-        case FCN_STEP:
-        case FCN_RUN:
-            coginit(cogid(), 0xf004, 0x4);
-            break;
-        case FCN_READ:
-            tx_packet();
-            break;
-        case FCN_WRITE:
-            rx_packet();
-            break;
+            case FCN_STEP:
+            case FCN_RUN:
+                coginit(cogid(), 0xf004, 0x4);
+                break;
+            case FCN_READ:
+                tx_packet();
+                break;
+            case FCN_WRITE:
+                rx_packet();
+                break;
         }
     }
-
+    
     return 0;
 }
 
@@ -89,7 +110,7 @@ static _NATIVE void hex(uint32_t value)
         txbyte((nibble < 10 ? '0' : 'a' - 10) + nibble);
         shift -= 4;
     } while (shift >= 0);
-
+    
     txbyte('\r');
     txbyte('\n');
 }
@@ -108,37 +129,37 @@ static _NATIVE void tx_packet(void)
 
 static _NATIVE void txbyte(int c)
 {
-  unsigned int waitcycles;
-  int i, value;
-
-  value = (c | 256) << 1;
-  waitcycles = _CNT + bitcycles;
-  for (i = 0; i < 10; i++)
+    unsigned int waitcycles;
+    int i, value;
+    
+    value = (c | 256) << 1;
+    waitcycles = _CNT + bitcycles;
+    for (i = 0; i < 10; i++)
     {
-      waitcycles = __builtin_propeller_waitcnt(waitcycles, bitcycles);
-      if (value & 1)
-        _OUTA |= txmask;
-      else
-        _OUTA &= ~txmask;
-      value >>= 1;
+        waitcycles = __builtin_propeller_waitcnt(waitcycles, bitcycles);
+        if (value & 1)
+            _OUTA |= txmask;
+        else
+            _OUTA &= ~txmask;
+        value >>= 1;
     }
 }
 
 int _NATIVE rxbyte(void)
 {
-  unsigned int waitcycles;
-  int i, value;
-
-  /* wait for a start bit */
-  __builtin_propeller_waitpeq(0, rxmask);
-
-  /* sync for one half bit */
-  waitcycles = _CNT + (bitcycles>>1) + bitcycles;
-  value = 0;
-  for (i = 0; i < 8; i++) {
-    waitcycles = __builtin_propeller_waitcnt(waitcycles, bitcycles);
-    value = ( (0 != (_INA & rxmask)) << 7) | (value >> 1);
-  }
-  __builtin_propeller_waitpeq(rxmask, rxmask);
-  return value;
+    unsigned int waitcycles;
+    int i, value;
+    
+    /* wait for a start bit */
+    __builtin_propeller_waitpeq(0, rxmask);
+    
+    /* sync for one half bit */
+    waitcycles = _CNT + (bitcycles>>1) + bitcycles;
+    value = 0;
+    for (i = 0; i < 8; i++) {
+        waitcycles = __builtin_propeller_waitcnt(waitcycles, bitcycles);
+        value = ( (0 != (_INA & rxmask)) << 7) | (value >> 1);
+    }
+    __builtin_propeller_waitpeq(rxmask, rxmask);
+    return value;
 }
