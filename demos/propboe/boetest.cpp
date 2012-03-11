c4b870ca15f9c2c212fcc0a0d3dc821858c2cb7f
David Betz <dbetz@xlisper.com>
2012-03-11 18:35:17 -0400

    Cleanup the test program so it works correctly on the C3.

--- demos/propboe/boetest.cpp
+++ demos/propboe/boetest.cpp
@@ -4,12 +4,23 @@
 #include "Term.h"
 
 #define C3
+//#define PROPBOE
 
-/* pin definitions on my PropBOE */
+/* pin definitions for C3 */
+#ifdef C3
+#define TV_PIN      12
+#define VGA_PIN     16
+#define VGA_ENABLE  15
+#endif
+
+/* pin definitions for the PropBOE */
+#ifdef PROPBOE
+#define VGA_PIN     8
 #define PIR_PIN     15
 #define PING_PIN    14
 #define LSERVO_PIN  16
 #define RSERVO_PIN  17
+#endif
 
 /* conversion factors for ping responses */
 #define US_PER_CM_1000      29033   // 29.033 * 1000
@@ -22,24 +33,43 @@ static void calibrate_servo(int pin);
 
 int main(void)
 {
-	TvTerm tv(12);
-	VgaTerm vga(16);
-    SerialTerm serial(stdout);
-    
-#ifdef C3
-	Pin vgaEnable(15);
+#ifdef TV_PIN
+    TvTerm tv(TV_PIN);
+	tv.str("Hello, world! (tv)\n");
+#endif
+
+#ifdef VGA_PIN
+    VgaTerm vga(VGA_PIN);
+	vga.str("Hello, world! (vga)\n");
+#endif
+
+#ifdef VGA_ENABLE
+	Pin vgaEnable(VGA_ENABLE);
 	vgaEnable.low();
 #endif
 
-	tv.str("Hello, world! (tv)\n");
-	vga.str("Hello, world! (vga)\n");
+    SerialTerm serial(stdout);
     serial.str("Hello, world! (serial)\n");
 
+#ifdef PIR_PIN
     //test_pir(PIR_PIN);
+#endif
+
+#ifdef PING_PIN
     //test_ping(PING_PIN);
-    calibrate_servo(LSERVO_PIN);
+#endif
+
+#ifdef LSERVO_PIN
+    //calibrate_servo(LSERVO_PIN);
+#endif
+
+#ifdef LSERVO_PIN
     //calibrate_servo(RSERVO_PIN);
-    
+#endif
+
+    for (;;)
+        ;
+
     return 0;
 }
 
