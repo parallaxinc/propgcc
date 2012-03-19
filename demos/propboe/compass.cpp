df979c9493edf3429fa352e7bd340ef9300b40d6
David Betz <dbetz@xlisper.com>
2012-03-19 17:34:13 -0400

    Add an I2C class.

--- demos/propboe/compass.cpp
+++ demos/propboe/compass.cpp
@@ -1,25 +1,23 @@
 #include <stdio.h>
 #include "Term.h"
-#include "boe.h"
+#include "I2C.h"
 
 #define COMPASS_ADDR    0x1e
 
-void CompassInit(I2C *dev);
-void CompassRead(I2C *dev, int *px, int *py, int *pz);
+void CompassInit(I2C &dev);
+void CompassRead(I2C &dev, int &x, int &y, int &z);
 
 int main(void)
 {
     SerialTerm serial(stdout);
-    I2C dev;
+    I2C bus(1, 0);
     
-    i2cInit(&dev, 1, 0);
-    
-    CompassInit(&dev);
+    CompassInit(bus);
 
     for (;;) {
         int x, y, z;
         
-        CompassRead(&dev, &x, &y, &z);
+        CompassRead(bus, x, y, z);
         serial.str("x ");
         serial.dec(x);
         serial.str(", y ");
@@ -34,40 +32,40 @@ int main(void)
     return 0;
 }
 
-void CompassInit(I2C *dev)
+void CompassInit(I2C &bus)
 {
     /* set to continuous mode */
-    i2cBegin(dev, COMPASS_ADDR);
-    i2cSend(dev, 0x02);
-    i2cSend(dev, 0x00);
-    i2cEnd(dev);
+    bus.begin(COMPASS_ADDR);
+    bus.send(0x02);
+    bus.send(0x00);
+    bus.end();
 }
 
-void CompassRead(I2C *dev, int *px, int *py, int *pz)
+void CompassRead(I2C &bus, int &x, int &y, int &z)
 {
     uint8_t data[6];
-    int16_t x, y, z;
+    int16_t x16, y16, z16;
     int i;
     
     /* select the data registers */
-    i2cBegin(dev, COMPASS_ADDR);
-    i2cSend(dev, 0x03);
-    i2cEnd(dev);
+    bus.begin(COMPASS_ADDR);
+    bus.send(0x03);
+    bus.end();
     
     /* prepare to read the data registers */
-    i2cRequest(dev, COMPASS_ADDR, 6);
+    bus.request(COMPASS_ADDR, 6);
     
     /* read the data registers */
     for (i = 0; i < 6; ++i)
-        data[i] = i2cReceive(dev);
+        data[i] = bus.receive();
         
     /* assemble the return values */
-    x = (data[0] << 8) | data[1];
-    z = (data[2] << 8) | data[3];
-    y = (data[4] << 8) | data[5];
+    x16 = (data[0] << 8) | data[1];
+    z16 = (data[2] << 8) | data[3];
+    y16 = (data[4] << 8) | data[5];
     
     /* return the signed values */
-    *px = x;
-    *py = y;
-    *pz = z;
+    x = x16;
+    y = y16;
+    z = z16;
 }
