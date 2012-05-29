ac58fbe32c4fc212f68ea21796562afe3353d004
David Betz <dbetz@xlisper.com>
2012-05-29 19:19:30 -0400

    Add a function to load COG images from high EEPROM.

---
+++ lib/include/cogload.h
@@ -0,0 +1,33 @@
+/**
+ * @file include/cogload.h
+ * @brief Provides Propeller specific functions to load COGs from EEPROM.
+ *
+ * Copyright (c) 2011-2012 by Parallax, Inc.
+ * MIT Licensed
+ */
+
+#ifndef __COGLOAD_H__
+#define __COGLOAD_H__
+
+/**
+ * @brief Start a new Propeller PASM COG from a COG image in EEPROM
+ *
+ * @details This is use to start a COG with code compiled as PASM, AS, or COG-C
+ * when the COG image is in EEPROM.
+ * PASM can be any Spin/PASM code that is self-contained. That is, all data
+ * for initialization and mailbox use are passed via the par parameter.
+ * Changing PASM variables from SPIN code will not work with this method.
+ *
+ * @details GAS and COG-C programs have similar restrictions.
+ * COG-C programs should not use any stack or variables in HUB memory that
+ * are not accessed via PAR mailbox or pointers.
+ *
+ * @param i2c Interface to an I2C bus
+ * @param code Address of PASM to load
+ * @param param Value of par parameter usually an address
+ * @returns COG ID provided by the builtin function or -1 on failure.
+ */
+int cognewFromEeprom(I2C *i2c, void *code, void *param);
+
+#endif
+
---
+++ lib/include/i2c.h
@@ -0,0 +1,168 @@
+/* i2c.h - definitions for a set of simple i2c functions
+
+Copyright (c) 2012 David Michael Betz
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
+#ifndef __I2C_H__
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+#include <stdint.h>
+#include "i2c_driver.h"
+
+/* forward type declarations */
+typedef struct I2C I2C;
+
+/* i2c operations */
+typedef struct {
+    int (*close)(I2C *dev);
+    int (*read)(I2C *dev, int address, uint8_t *buffer, int count, int stop);
+    int (*write)(I2C *dev, int address, uint8_t *buffer, int count, int stop);
+} I2C_OPS;
+
+/* i2c state information */
+struct I2C {
+    I2C_OPS *ops;
+};
+
+typedef struct {
+    I2C i2c;
+    int cog;
+    volatile I2C_MAILBOX mailbox;
+} I2C_COGDRIVER;
+
+typedef struct {
+    I2C i2c;
+    uint32_t scl_mask;
+    uint32_t sda_mask;
+} I2C_SIMPLE;
+
+/**
+ * @brief Open an I2C device
+ *
+ * @details Open an I2C device using a COG driver.
+ *
+ * @param dev I2C device structure to initialize
+ * @param scl SCL pin number
+ * @param sda SDA pin number
+ * @param freq Bus frequency
+ *
+ * @returns a pointer to the device on success, NULL on failure.
+ *
+ */
+I2C *i2cOpen(I2C_COGDRIVER *dev, int scl, int sda, int freq);
+
+/**
+ * @brief Return a pointer to the I2C COG driver image
+ *
+ * @details Returns a pointer to the I2C COG driver image.
+ * This allows the space that was occupied by the COG driver
+ * to be reused after the driver has been loaded. Call this
+ * only after the final call to i2cOpen.
+ *
+ * @returns a pointer to the I2C COG driver image.
+ *
+ */
+void *i2cGetCogBuffer(void);
+
+/**
+ * @brief Open an I2C device
+ *
+ * @details Open an I2C device using a simple driver that
+ * runs on the COG of the caller.
+ *
+ * @param dev I2C device structure to initialize
+ * @param scl SCL pin number
+ * @param sda SDA pin number
+ *
+ * @returns a pointer to the device on success, NULL on failure.
+ *
+ */
+I2C *simple_i2cOpen(I2C_SIMPLE *dev, int scl, int sda);
+
+/**
+ * @brief Close an I2C device
+ *
+ * @details Close an I2C device that was opened with
+ * either i2cOpen or simple_i2cOpen.
+ *
+ * @param dev I2C device to close
+ *
+ * @returns 0 on success, -1 on failure.
+ *
+ */
+static inline int i2cClose(I2C *dev)
+{
+    return (*dev->ops->close)(dev);
+}
+
+/**
+ * @brief Write to an I2C device
+ *
+ * @details Write to an I2C device at the specified address.
+ * The address should be the device address in bits 7:1 and
+ * a zero in bit 0. If count is zero only the address byte
+ * will be sent. Set the stop parameter to TRUE to cause an
+ * I2C stop sequence to be emitted after the data. Setting it
+ * to FALSE omits the stop sequence.
+ *
+ * @param dev I2C device to close
+ * @param address I2C address in bits 7:1, zero in bit 0
+ * @param buffer Address of the buffer containing data to write
+ * @param count Number of bytes of data to write
+ * @param stop TRUE to send a stop sequence after the data
+ *
+ * @returns 0 on success, -1 on failure.
+ *
+ */
+static inline int i2cWrite(I2C *dev, int address, uint8_t *buffer, int count, int stop)
+{
+    return (*dev->ops->write)(dev, address, buffer, count, stop);
+}
+
+/**
+ * @brief Read from an I2C device
+ *
+ * @details Read from an I2C device at the specified address.
+ * The address should be the device address in bits 7:1 and
+ * a zero in bit 0. Set the stop parameter to TRUE to cause an
+ * I2C stop sequence to be emitted after the data. Setting it
+ * to FALSE omits the stop sequence.
+ *
+ * @param dev I2C device to close
+ * @param address I2C address in bits 7:1, zero in bit 0
+ * @param buffer Address of the buffer to receive data
+ * @param count Number of bytes of data to receive
+ * @param stop TRUE to send a stop sequence after the data
+ *
+ * @returns 0 on success, -1 on failure.
+ *
+ */
+static inline int i2cRead(I2C *dev, int address, uint8_t *buffer, int count, int stop)
+{
+    return (*dev->ops->read)(dev, address, buffer, count, stop);
+}
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif
