f24514bad8fde0199da4ea7ec526041baa333eb1
David Betz <dbetz@xlisper.com>
2012-06-17 14:15:14 -0400

    Remove the ebasic2 project because development has moved to the Google Code project pico-basic.

--- demos/ebasic2/db_vmdebug.h
+++
@@ -1,9 +0,0 @@
-#ifndef __DB_VMDEBUG_H__
-#define __DB_VMDEBUG_H__
-
-#include "db_types.h"
-
-void DecodeFunction(VMUVALUE offset, const uint8_t *code, int len);
-int DecodeInstruction(VMUVALUE base, const uint8_t *code, const uint8_t *lc);
-
-#endif
