0ca32cc9afc89e9cf0748e29371bd94ce3589f3c
Eric Smith <ersmith@totalspectrum.ca>
2012-07-16 20:41:08 -0300

    add C++ wrapper

--- lib/include/setjmp.h
+++ lib/include/setjmp.h
@@ -3,9 +3,17 @@
 
 #include <sys/jmpbuf.h>
 
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
 typedef _jmp_buf jmp_buf;
 
 void longjmp(jmp_buf env, int val);
 int  setjmp(jmp_buf env);
+
+#if defined(__cplusplus)
+}
+#endif
 
 #endif
