18b057da2a19c99d0a4b08a04595eb03d3dc9c75
David Betz <dbetz@xlisper.com>
2012-09-08 22:31:35 -0400

    Add the beginnings of the shunting yard algorithm. It doesn't handle unary operators yet though.

--- demos/expr/expr.c
+++ demos/expr/expr.c
@@ -1,3 +1,5 @@
+#define USE_SHUNTING_YARD_ALGORITHM
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
@@ -27,15 +29,259 @@
 #define TYPE_NUMBER     1
 #define TYPE_VARIABLE   2
 
-typedef struct {
-    int type;
-    union {
-        VALUE value;
-        Variable *var;
-    } v;
-} PVAL;
-
 static void RValue(EvalState *c, PVAL *pval);
+static int GetToken(EvalState *c, PVAL *pval);
+static void ParseIdentifier(EvalState *c, PVAL *pval);
+static void ParseNumber(EvalState *c, PVAL *pval);
+static int AddVariable(EvalState *c, char *id, PVAL *pval);
+static Variable *FindVariable(EvalState *c, char *id);
+static void Error(EvalState *c, const char *fmt, ...);
+
+/* InitEvalState - initialize the expression evaluator state */
+void InitEvalState(EvalState *c, uint8_t *heap, size_t heapSize)
+{
+    memset(c, 0, sizeof(EvalState));
+    c->oStackTop = (int *)((char *)c->oStack + sizeof(c->oStack));
+    c->rStackTop = (PVAL *)((char *)c->rStack + sizeof(c->rStack));
+    c->base = heap;
+    c->free = heap;
+    c->top = heap + heapSize;
+}
+
+#ifdef USE_SHUNTING_YARD_ALGORITHM
+
+/* operator associativities */
+#define ASSOC_LEFT      1
+#define ASSOC_RIGHT     2
+
+/* operator stack macros */
+#define oStackIsEmpty(c)    ((c)->oStackPtr < (c)->oStack)
+#define oStackPush(c,v)     do {                                            \
+                                if ((c)->oStackPtr >= (c)->oStackTop)       \
+                                    Error(c, "operator stack overflow");    \
+                                *++(c)->oStackPtr = (v);                    \
+                            } while (0)
+#define oStackPop(c)        (*(c)->oStackPtr--)
+#define oStackTop(c)        (*(c)->oStackPtr)
+                        
+/* operand stack macros */
+#define rStackIsEmpty(c)    ((c)->rStackPtr < (c)->rStack)
+#define rStackPush(c,v)     do {                                            \
+                                if ((c)->rStackPtr >= (c)->rStackTop)       \
+                                    Error(c, "operand stack overflow");     \
+                                *++(c)->rStackPtr = (v);                    \
+                            } while (0)
+#define rStackDrop(c)       ((c)->rStackPtr--)
+
+static int Prec(EvalState *c, int op);
+static int Assoc(EvalState *c, int op);
+static void Apply(EvalState *c, int op, PVAL *left, PVAL *right);
+
+/* EvalExpr - Eval and evaluate an expression using the shunting yard algorithm */
+int EvalExpr(EvalState *c, const char *str, VALUE *pValue)
+{
+    PVAL pval;
+    int tkn;
+    
+    /* setup an error target */
+    if (setjmp(c->errorTarget))
+        return FALSE;
+        
+    /* initialize the parser */
+    c->linePtr = (char *)str;
+    c->savedToken = TKN_NONE;
+    
+    /* initialize the operator and operand stacks */
+    c->oStackPtr = c->oStack - 1;
+    c->rStackPtr = c->rStack - 1;
+    
+    /* handle each input token */
+    while ((tkn = GetToken(c, &pval)) != TKN_EOF) {
+        int prec;
+        switch (tkn) {
+        case TKN_IDENTIFIER:
+        case TKN_NUMBER:
+            rStackPush(c, pval);
+            break;
+        case '(':
+            oStackPush(c, tkn);
+            break;
+        case ')':
+            for (;;) {
+                PVAL *left, *right;
+                int op;
+                if (oStackIsEmpty(c))
+                    Error(c, "mismatched parens");
+                if ((op = oStackPop(c)) == '(')
+                    break;
+                left = &c->rStackPtr[-1];
+                right = &c->rStackPtr[0];
+                Apply(c, op, left, right);
+                rStackDrop(c);
+            }
+            break;
+        default:
+            prec = Prec(c, tkn);
+            while (!oStackIsEmpty(c)) {
+                int stackPrec = Prec(c, oStackTop(c));
+                PVAL *left, *right;
+                int op;
+                if ((Assoc(c, tkn) == ASSOC_LEFT && prec > stackPrec) || prec >= stackPrec)
+                    break;
+                op = oStackPop(c);
+                left = &c->rStackPtr[-1];
+                right = &c->rStackPtr[0];
+                Apply(c, op, left, right);
+                rStackDrop(c);
+            }
+            oStackPush(c, tkn);
+            break;
+        }
+    }
+    
+    /* apply all of the remaining operands on the operator stack */
+    while (!oStackIsEmpty(c)) {
+        int op = oStackPop(c);
+        if (op == '(')
+            Error(c, "mismatched parens");
+        PVAL *left = &c->rStackPtr[-1];
+        PVAL *right = &c->rStackPtr[0];
+        Apply(c, op, left, right);
+        rStackDrop(c);
+    }
+    
+    RValue(c, &c->rStackPtr[0]);
+    *pValue = c->rStackPtr[0].v.value;
+    
+    /* return successfully */
+    return TRUE;
+}
+
+/* Prec - return the precedence of an operator */
+static int Prec(EvalState *c, int op)
+{
+    int precedence;
+    switch (op) {
+    case '(':
+        precedence = 0;
+        break;
+    case '=':
+        precedence = 1;
+        break;
+    case '|':
+        precedence = 2;
+        break;
+    case '^':
+        precedence = 3;
+        break;
+    case '&':
+        precedence = 4;
+        break;
+    case TKN_SHL:
+    case TKN_SHR:
+        precedence = 5;
+        break;
+    case '+':
+    case '-':
+        precedence = 6;
+        break;
+    case '*':
+    case '/':
+    case '%':
+        precedence = 7;
+        break;
+    default:
+        Error(c, "unknown operator -- '%c'", op);
+        break;
+    }
+    return precedence;
+}
+
+/* Assoc - return the associativity of an operator */
+static int Assoc(EvalState *c, int op)
+{
+    int associativity;
+    switch (op) {
+    case '=':
+        associativity = ASSOC_RIGHT;
+        break;
+    case '|':
+    case '^':
+    case '&':
+    case TKN_SHL:
+    case TKN_SHR:
+    case '+':
+    case '-':
+    case '*':
+    case '/':
+    case '%':
+        associativity = ASSOC_LEFT;
+        break;
+    default:
+        Error(c, "unknown operator -- '%c'", op);
+        break;
+    }
+    return associativity;
+}
+
+/* Apply - apply an operator to operands */
+static void Apply(EvalState *c, int op, PVAL *left, PVAL *right)
+{
+    RValue(c, right);
+    if (op == '=') {
+        if (left->type != TYPE_VARIABLE)
+            Error(c, "expecting a variable to the left of '='");
+        left->v.var->value = right->v.value;
+        left->v.var->bound = TRUE;
+        left->type = TYPE_NUMBER;
+        left->v.value = right->v.value;
+    }
+    else {
+        RValue(c, left);
+        switch (op) {
+        case '|':
+            left->v.value = (VALUE)((int)left->v.value | (int)right->v.value);
+            break;
+        case '^':
+            left->v.value = (VALUE)((int)left->v.value ^ (int)right->v.value);
+            break;
+        case '&':
+            left->v.value = (VALUE)((int)left->v.value & (int)right->v.value);
+            break;
+        case TKN_SHL:
+            left->v.value = (VALUE)((int)left->v.value << (int)right->v.value);
+            break;
+        case TKN_SHR:
+            left->v.value = (VALUE)((int)left->v.value >> (int)right->v.value);
+            break;
+        case '+':
+            left->v.value += right->v.value;
+            break;
+        case '-':
+            left->v.value -= right->v.value;
+            break;
+        case '*':
+            left->v.value *= right->v.value;
+            break;
+        case '/':
+            if ((int)right->v.value == 0)
+                Error(c, "division by zero");
+            left->v.value /= right->v.value;
+            break;
+        case '%':
+            if ((int)left->v.value == 0)
+                Error(c, "division by zero");
+            left->v.value = (VALUE)((int)left->v.value % (int)right->v.value);
+            break;
+        default:
+            Error(c, "internal error");
+            break;
+        }
+    }
+}
+
+#else   // !USE_SHUNTING_YARD_ALGORITHM
+
 static void EvalExpr1(EvalState *c, PVAL *pval);
 static void EvalExpr2(EvalState *c, PVAL *pval);
 static void EvalExpr3(EvalState *c, PVAL *pval);
@@ -45,21 +291,6 @@ static void EvalExpr6(EvalState *c, PVAL *pval);
 static void EvalExpr7(EvalState *c, PVAL *pval);
 static void EvalExpr8(EvalState *c, PVAL *pval);
 static void EvalPrimary(EvalState *c, PVAL *pval);
-static int GetToken(EvalState *c, PVAL *pval);
-static void EvalIdentifier(EvalState *c, PVAL *pval);
-static void EvalNumber(EvalState *c, PVAL *pval);
-static int AddVariable(EvalState *c, char *id, PVAL *pval);
-static Variable *FindVariable(EvalState *c, char *id);
-static void Error(EvalState *c, const char *fmt, ...);
-
-/* InitEvalState - initialize the expression evaluator state */
-void InitEvalState(EvalState *c, uint8_t *heap, size_t heapSize)
-{
-    memset(c, 0, sizeof(EvalState));  
-    c->base = heap;
-    c->free = heap;
-    c->top = heap + heapSize;
-}
 
 /* EvalExpr - Eval and evaluate an expression */
 int EvalExpr(EvalState *c, const char *str, VALUE *pValue)
@@ -87,23 +318,6 @@ int EvalExpr(EvalState *c, const char *str, VALUE *pValue)
         
     /* return successfully */
     return TRUE;
-}
-
-static void RValue(EvalState *c, PVAL *pval)
-{
-    switch (pval->type) {
-    case TYPE_NUMBER:
-        /* nothing to do */
-        break;
-    case TYPE_VARIABLE:
-        if (!pval->v.var->bound)
-            Error(c, "%s has no value", pval->v.var->name);
-        pval->type = TYPE_NUMBER;
-        pval->v.value = pval->v.var->value;
-        break;
-    default:
-        Error(c, "internal error");
-    }
 }
 
 /* EvalExpr1 - handle the '=' operator */
@@ -271,11 +485,6 @@ static void EvalExpr8(EvalState *c, PVAL *pval)
         RValue(c, pval);
         pval->v.value = (VALUE)~(int)pval->v.value;
         break;
-    case '!':
-        EvalPrimary(c, pval);
-        RValue(c, pval);
-        pval->v.value = !pval->v.value;
-        break;
     default:
         c->savedToken = tkn;
         EvalPrimary(c, pval);
@@ -303,6 +512,25 @@ static void EvalPrimary(EvalState *c, PVAL *pval)
     }
 }
 
+#endif  // USE_SHUNTING_YARD_ALGORITHM
+
+static void RValue(EvalState *c, PVAL *pval)
+{
+    switch (pval->type) {
+    case TYPE_NUMBER:
+        /* nothing to do */
+        break;
+    case TYPE_VARIABLE:
+        if (!pval->v.var->bound)
+            Error(c, "%s has no value", pval->v.var->name);
+        pval->type = TYPE_NUMBER;
+        pval->v.value = pval->v.var->value;
+        break;
+    default:
+        Error(c, "internal error");
+    }
+}
+
 static int GetToken(EvalState *c, PVAL *pval)
 {
     int tkn;
@@ -322,13 +550,13 @@ static int GetToken(EvalState *c, PVAL *pval)
                     
     /* check for a number */
     if (*c->linePtr == '.' || isdigit(*c->linePtr)) {
-        EvalNumber(c, pval);
+        ParseNumber(c, pval);
         tkn = TKN_NUMBER;
     }
     
     /* check for an identifier */
     else if (isalpha(*c->linePtr)) {
-        EvalIdentifier(c, pval);
+        ParseIdentifier(c, pval);
         tkn = TKN_IDENTIFIER;
     }
     
@@ -389,13 +617,13 @@ static int GetToken(EvalState *c, PVAL *pval)
     return tkn;
 }
 
-static void EvalNumber(EvalState *c, PVAL *pval)
+static void ParseNumber(EvalState *c, PVAL *pval)
 {
     pval->type = TYPE_NUMBER;
     pval->v.value = strtod(c->linePtr, (char **)&c->linePtr);
 }
 
-static void EvalIdentifier(EvalState *c, PVAL *pval)
+static void ParseIdentifier(EvalState *c, PVAL *pval)
 {
     char id[ID_MAX];
     char *p = id;
--- demos/expr/expr.h
+++ demos/expr/expr.h
@@ -4,17 +4,30 @@
 #include <stdint.h>
 #include <setjmp.h>
 
+/* forward type definitions */
+typedef struct Variable Variable;
+
 /* value type */
 typedef double VALUE;
 
+typedef struct {
+    int type;
+    union {
+        VALUE value;
+        Variable *var;
+    } v;
+} PVAL;
+
 /* variable structure */
-typedef struct Variable Variable;
 struct Variable {
     Variable *next;
     int bound;
     VALUE value;
     char name[1];
 };
+
+#define OSTACK_SIZE 16
+#define RSTACK_SIZE 16
 
 /* parse context */
 typedef struct {
@@ -23,6 +36,14 @@ typedef struct {
     jmp_buf errorTarget;
     char *linePtr;
     int savedToken;
+#ifdef USE_SHUNTING_YARD_ALGORITHM
+    int oStack[OSTACK_SIZE];
+    int *oStackPtr;
+    int *oStackTop;
+    PVAL rStack[RSTACK_SIZE];
+    PVAL *rStackPtr;
+    PVAL *rStackTop;
+#endif
     Variable *variables;
     uint8_t *base;      /* base of heap data */
     uint8_t *free;      /* next free heap location */
