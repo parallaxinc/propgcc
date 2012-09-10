3de5fe13f6c493a7fe7c7d08a3d969dd6cccba27
Eric Smith <ersmith@totalspectrum.ca>
2012-09-10 07:26:25 -0300

    do not use GNU libgomp, use our own

--- jbuild.sh
+++ jbuild.sh
@@ -170,7 +170,7 @@ cd ../../propgcc
 #
 mkdir -p ../build/gcc
 cd ../build/gcc
-../../propgcc/gcc/configure --target=propeller-elf --prefix=$PREFIX --disable-nls --disable-libssp --disable-lto --disable-shared --enable-libgomp ${CONFIG_OPTIONS}
+../../propgcc/gcc/configure --target=propeller-elf --prefix=$PREFIX --disable-nls --disable-libssp --disable-lto --disable-shared ${CONFIG_OPTIONS}
 if test $? != 0
 then
    echo "gcc configure failed."
