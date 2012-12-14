7c8bd124c2eed368291c4eb7137d8417612c4b9e
David Betz <dbetz@xlisper.com>
2012-12-13 21:19:46 -0500

    Add the reps instruction.

--- binutils/opcodes/propeller-opc.c
+++ binutils/opcodes/propeller-opc.c
@@ -358,6 +358,8 @@ const struct propeller_opcode propeller_opcodes[] = {
   {"coginit", 0x0c000000, 0xfc400000, PROPELLER_OPERAND_TWO_OPS, NR, PROP_2, NO_COMPRESSED, 0},
 /* getcnt    000011 zc11 cccc ddddddddd 000001101 */
   {"getcnt", 0x0cc0000d, 0xfcc001ff, PROPELLER_OPERAND_DEST_ONLY, R, PROP_2, NO_COMPRESSED, 0},
+/* reps  000011 n111 nnnn nnnnnnnnn 001iiiiii */
+  {"reps", 0x0dc00040, 0xfdc001c0, PROPELLER_OPERAND_REPS, NR, PROP_2, NO_COMPRESSED, 0},
 };
 
 const int propeller_num_opcodes =
