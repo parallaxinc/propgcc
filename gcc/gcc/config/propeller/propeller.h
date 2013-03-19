dad6f6647c1485141dcc9e59854ab6553653a992
Eric Smith <ersmith@totalspectrum.ca>
2013-03-19 12:16:55 -0300

    fix build specs for -mcog -mp2 case

--- gcc/gcc/config/propeller/propeller.h
+++ gcc/gcc/config/propeller/propeller.h
@@ -52,8 +52,8 @@
 #undef LINK_SPEC
 #define LINK_SPEC "                                             \
 %{mrelax:-relax}                                                \
-%{mcog:-mpropeller_cog; mxmmc:-mpropeller_xmmc; mxmm:-mpropeller_xmm; mxmm-single:-mpropeller_xmm_single; mxmm-split:-mpropeller_xmm_split; :-mpropeller}  \
-%{mp2:-mpropeller2} \
+%{mp2: %{mcog:-mpropeller_cog; :-mpropeller2};                   \
+  : %{mcog:-mpropeller_cog; mxmmc:-mpropeller_xmmc; mxmm:-mpropeller_xmm; mxmm-single:-mpropeller_xmm_single; mxmm-split:-mpropeller_xmm_split; :-mpropeller}} \
 "
 
 #define TARGET_DEFAULT (MASK_LMM | MASK_64BIT_DOUBLES)
