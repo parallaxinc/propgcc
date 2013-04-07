0ca9d72d7dcf80f91abcddd3016cb7172e7e5520
Steve Denson <jsdenson@gmail.com>
2013-04-07 10:57:19 -0700

    Fix missing include code.

--- lib/include/string.h
+++ lib/include/string.h
@@ -22,6 +22,7 @@ int    strcoll(const char *s1, const char *s2);
 int    strncmp(const char *s1, const char *s2, size_t n);
 size_t strxfrm(char *dest, const char *src, size_t n);
 
+char * strdup (const char *s1);
 
 void * memchr(const void *s, int c, size_t n);
 char * strchr(const char *, int);
