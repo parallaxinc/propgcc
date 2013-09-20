6002c951bce43af8d58a2addd3f834a573ba8585
David Betz <dbetz@xlisper.com>
2013-09-19 22:33:42 -0400

    Add .start.kerext preloading to PEX file images.

--- loader/src/pex-image.c
+++ loader/src/pex-image.c
@@ -11,7 +11,7 @@
 
 int WriteExecutableFile(char *path, BoardConfig *config, ElfContext *c, char *outfile)
 {
-    ElfProgramHdr program_kernel;
+    ElfProgramHdr program_kernel, program_start;
     uint8_t *imagebuf, *buf;
     uint32_t loadAddress;
     PexeFileHdr hdr;
@@ -29,19 +29,38 @@ int WriteExecutableFile(char *path, BoardConfig *config, ElfContext *c, char *ou
     }
         
     /* find the .xmmkernel segment */
-    if (FindProgramSegment(c, ".xmmkernel", &program_kernel) < 0)
+    if (FindProgramSegment(c, ".xmmkernel", &program_kernel) < 0) {
+        free(imagebuf);
         return Error("can't find .xmmkernel segment");
+    }
     
     /* load the .kernel section */
-    if (!(buf = LoadProgramSegment(c, &program_kernel)))
+    if (!(buf = LoadProgramSegment(c, &program_kernel))) {
+        free(imagebuf);
         return Error("can't load .xmmkernel section");
+    }
 
     memset(&hdr, 0, sizeof(hdr));
     strcpy(hdr.tag, PEXE_TAG);
     hdr.version = PEXE_VERSION;
     hdr.loadAddress = loadAddress;
     memcpy(hdr.kernel, buf, program_kernel.filesz);
+    free(buf);
     
+    /* find the .start.kerext segment */
+    if (FindProgramSegment(c, ".start.kerext", &program_start) >= 0) {
+    
+        /* load the .start.kerext section */
+        if (!(buf = LoadProgramSegment(c, &program_start))) {
+            free(imagebuf);
+            return Error("can't load .start.kerext section");
+        }
+        
+        /* preload .start.kerext */
+        memcpy(&hdr.kernel[program_start.vaddr / sizeof(uint32_t)], buf, program_start.filesz);
+        free(buf);
+    }
+
     ConstructOutputName(outfile, path, ".pex");
     if (!(fp = fopen(outfile, "wb"))) {
         free(imagebuf);
