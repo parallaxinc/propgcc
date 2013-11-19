caf4e30052e286383d2a9a098968a451593f073d
Eric Smith <ersmith@totalspectrum.ca>
2013-11-19 11:10:16 -0400

    fixed _CLKFREQ access from COGC

--- gcc/gcc/config/propeller/spinboot.s
+++ gcc/gcc/config/propeller/spinboot.s
@@ -10,8 +10,16 @@
 	'' which in the ROM has a stop routine)
 	''
 	.section .boot, "ax", @progbits
-	.global __clkfreq
-	.global __clkmode
+
+	'' These variables used to be .global, but are now defined in
+	'' the linker script. This is because the .boot section is
+	'' discarded when doing a relocatable link (which is how we
+	'' often build COGC programs), which will cause a program using
+	'' these symbols from the .boot sections to get a linker error.
+	'' Defining them in the linker script avoids the issue.
+	
+	''.global __clkfreq
+	''.global __clkmode
 	.extern __hub_end
 	.extern __stack_end
 start
@@ -52,40 +60,3 @@ pbase
 
 '' here is where the pasm code actually goes
 
-	''
-	'' and finally some definitions for the standard
-	'' COG registers
-	''
-	.global PAR
-	.global CNT
-	.global INA
-	.global INB
-	.global OUTA
-	.global OUTB
-	.global DIRA
-	.global DIRB
-	.global CTRA
-	.global CTRB
-	.global FRQA
-	.global FRQB
-	.global PHSA
-	.global PHSB
-	.global VCFG
-	.global VSCL
-
-	PAR = (4*0x1F0)
-	CNT = (4*0x1F1)
-	INA = (4*0x1F2)
-	INB = (4*0x1F3)
-	OUTA = (4*0x1F4)
-	OUTB = (4*0x1F5)
-	DIRA = (4*0x1F6)
-	DIRB = (4*0x1F7)
-	CTRA = (4*0x1F8)
-	CTRB = (4*0x1F9)
-	FRQA = (4*0x1FA)
-	FRQB = (4*0x1FB)
-	PHSA = (4*0x1FC)
-	PHSB = (4*0x1FD)
-	VCFG = (4*0x1FE)
-	VSCL = (4*0x1FF)
