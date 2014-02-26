6fe867c4b9e77db0ee7a0e4ddb303a8c0b65d694
David Betz <dbetz@xlisper.com>
2014-02-26 07:06:57 -0500

    Handle the case where a Spin object has no variables. Add a test of returning values from Spin functions.

--- demos/spinwrap/test_main.cpp
+++ demos/spinwrap/test_main.cpp
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "test.h"
 
 #define QUICKSTART
@@ -9,21 +10,25 @@ int main(void)
     test obj;
     test obj2;
 
+    obj.set_pins(16, 17);
+    obj2.set_pins(20, 21);
+
+    printf("obj: pin %d, other pin %d\n", obj.get_pin(), obj.get_other_pin());
+    printf("obj2: pin %d, other pin %d\n", obj2.get_pin(), obj2.get_other_pin());
+
     obj.start_blinker();
     
-    obj.set_pins(16, 17);
     obj.blink();
     obj.set_pin(18);
     obj.blink();
 
-    obj2.set_pins(20, 21);
     obj2.blink();
     obj2.set_pin(22);
     obj2.blink();
     
     while (1) {
-    	obj.blink();
-    	obj2.blink();
+        obj.blink();
+        obj2.blink();
     }
 
 #endif
@@ -33,7 +38,7 @@ int main(void)
     
     obj.set_pins(26, 27);
     while (1)
-    	obj.blink();
+        obj.blink();
 #endif
 
     return 0;
